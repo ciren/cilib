ee3399c9b927d3e8516460a912766681eba4f852
Gary Pampara <gpampara@gmail.com>
2006-03-09 08:09:00 +0000

    r269@balrog:  gpampara | 2006-03-09 10:10:25 +0200  Corrected some of the JavaDoc and the modifier for a class member variable
    git-svn-id: http://cilib.svn.sourceforge.net/svnroot/cilib/trunk@249 6919081d-ccfc-0310-b8b8-fc61df93b961

--- src/net/sourceforge/cilib/problem/dataset/DataSet.java
+++ src/net/sourceforge/cilib/problem/dataset/DataSet.java
@@ -38,7 +38,8 @@ import java.io.InputStream;
  */
 public abstract class DataSet {
 	
-	String patternExpression = "";
+	private String patternExpression = "";
+	
 	/**
 	 * Returns the data set as a byte array.
 	 * 
@@ -54,8 +55,17 @@ public abstract class DataSet {
 	public abstract InputStream getInputStream();
 	
 	/**
-	 * Set the regular expression that will be used to split the patterns in the provided DataSet file.
-	 * @param regexp The format of this regular expression depends on where you are calling the method from. When you specify the regular expression in an XML file, the format should be a standard regular expression. When you call this method directly with a regular expression in double quotes (from a Java source file), then the format of the regular expression should be a Java style regular expression.
+	 * Set the regular expression that will be used to split the patterns in the 
+	 * provided DataSet file.
+	 * 
+	 * The format of this regular expression depends on where you are calling the 
+	 * method from. When you specify the regular expression in a simulation XML file,
+	 * the format should be a standard regular expression. When you call this method 
+	 * directly with a regular expression in double quotes (from a Java source file),
+	 * then the format of the regular expression should be a Java style regular 
+	 * expression.
+	 * 
+	 * @param regexp The regex to use 
 	 */
 	public void setPatternExpression(String regexp) {
 		patternExpression = regexp;
@@ -63,7 +73,8 @@ public abstract class DataSet {
 	
 	/**
 	 * Get the regular expression that has been set for this DataSet.
-	 * @return a string containing the regular expression.
+	 * 
+	 * @return The regular expression.
 	 */
 	public String getPatternExpression() {
 		return patternExpression;
