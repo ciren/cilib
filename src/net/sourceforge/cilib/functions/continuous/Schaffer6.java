785586a517ba31ad6993fc2f29ac0f7b1a78b4f4
Gary Pampara <gpampara@gmail.com>
2006-03-14 07:47:25 +0000

    r271@balrog:  gpampara | 2006-03-14 09:48:57 +0200  Fixed a bug with the Schaffer6 function. It is now 100% correct :)
    git-svn-id: http://cilib.svn.sourceforge.net/svnroot/cilib/trunk@250 6919081d-ccfc-0310-b8b8-fc61df93b961

--- src/net/sourceforge/cilib/functions/continuous/Schaffer6.java
+++ src/net/sourceforge/cilib/functions/continuous/Schaffer6.java
@@ -62,7 +62,7 @@ public class Schaffer6 extends ContinuousFunction {
 	 * @return The minimum value as a <tt>Double</tt> object with value of 0.0 
 	 */
 	public Object getMinimum() {
-		return new Double(0.0);
+		return new Double(-1.0);
 	}
 
 	
@@ -84,7 +84,7 @@ public class Schaffer6 extends ContinuousFunction {
 		double denominatorTmp = 1.0 + 0.001*squared;
 		double denominator = denominatorTmp * denominatorTmp;
 
-		return 0.5 + (numerator / denominator);
+		return -0.5 + (numerator / denominator);
 	}
 
 }
