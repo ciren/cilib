cad0cb4ab90428563acda505ffde9c00aacd8825
Gary Pampara <gpampara@gmail.com>
2006-05-17 07:26:43 +0000

    Initial commit of the Initialise annotation that will be used for object initialisation within CILib.
    git-svn-id: http://cilib.svn.sourceforge.net/svnroot/cilib/trunk@317 6919081d-ccfc-0310-b8b8-fc61df93b961

---
+++ src/net/sourceforge/cilib/annotations/Initialise.java
@@ -0,0 +1,14 @@
+package net.sourceforge.cilib.annotations;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.annotation.ElementType;
+
+@Documented
+@Target({ ElementType.METHOD })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Initialise {
+
+}
