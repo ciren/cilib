63efbaa09a49c0f04254a8b96c854b4f490d26b6
Gary Pampara <gpampara@gmail.com>
2006-08-25 08:19:08 +0000

    Added another test case
    git-svn-id: http://cilib.svn.sourceforge.net/svnroot/cilib/trunk@347 6919081d-ccfc-0310-b8b8-fc61df93b961

---
+++ tests/net/sourceforge/cilib/entity/IndividualTest.java
@@ -0,0 +1,38 @@
+package net.sourceforge.cilib.entity;
+
+import static org.junit.Assert.assertEquals;
+import net.sourceforge.cilib.ec.ea.Individual;
+import net.sourceforge.cilib.type.types.MixedVector;
+import net.sourceforge.cilib.type.types.Real;
+import net.sourceforge.cilib.type.types.Vector;
+
+import org.junit.Test;
+
+public class IndividualTest {
+	
+	@Test
+	public void testClone() {
+		Vector genes = new MixedVector(5);
+		genes.add(new Real(1.0));
+		genes.add(new Real(2.0));
+		genes.add(new Real(3.0));
+		genes.add(new Real(4.0));
+		genes.add(new Real(5.0));
+		
+		Individual i = new Individual();
+		i.set(genes);
+		
+		Individual clone = i.clone();
+		
+		assertEquals(5, i.getDimension());
+		assertEquals(5, clone.getDimension());
+		assertEquals(i.getDimension(), clone.getDimension());
+		
+		Vector cloneVector = (Vector) clone.get();
+		
+		for (int k = 0; k < cloneVector.getDimension(); k++) {
+			assertEquals(genes.get(k), cloneVector.get(k));
+		}
+	}
+
+}
