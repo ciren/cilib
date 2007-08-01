1f91cb34ca04966feac251877f6746e466d49869
Theuns Cloete <thenus.cloete@gmail.com>
2007-08-01 06:28:18 +0000

    r532@pavilion:  heatzync | 2007-07-31 08:38:46 +0200  Cleaned up the CachedDistanceDataSetBuilder class a bit. I'm going to rename it after this anyways ;-) because this class does not ONLY cache the distances between patterns anymore. The dataset mean and dataset variance is also cached.
    git-svn-id: http://cilib.svn.sourceforge.net/svnroot/cilib/trunk@487 6919081d-ccfc-0310-b8b8-fc61df93b961

--- src/main/java/net/sourceforge/cilib/problem/dataset/CachedDistanceDataSetBuilder.java
+++ src/main/java/net/sourceforge/cilib/problem/dataset/CachedDistanceDataSetBuilder.java
@@ -1,11 +1,15 @@
 package net.sourceforge.cilib.problem.dataset;
 
+import org.apache.log4j.Logger;
+
 import net.sourceforge.cilib.type.types.container.Vector;
 
 public class CachedDistanceDataSetBuilder extends AssociatedPairDataSetBuilder {
 	private static final long serialVersionUID = -8098125665317527403L;
-	
-	private Vector mean = null, variance = null;
+	private static Logger log = Logger.getLogger(CachedDistanceDataSetBuilder.class);
+
+	private Vector mean = null;
+	private Vector variance = null;
 	private double distanceCache[] = null;
 	private int cacheSize = 0;
 
@@ -25,32 +29,35 @@ public class CachedDistanceDataSetBuilder extends AssociatedPairDataSetBuilder {
 	}
 
 	private void cacheDistances() {
+		log.info("Caching distances between patterns");
 		int numPatterns = patterns.size();
 		cacheSize = (numPatterns * (numPatterns - 1)) / 2;
 		distanceCache = new double[cacheSize];
 		int index = 0;
-		for(int y = 0; y < numPatterns; y++) {
+		for(int y = 0; y < numPatterns - 1; y++) {
 			for(int x = y + 1; x < numPatterns; x++) {
 				index = x + (numPatterns * y) - (((y + 1) * (y + 2)) / 2);
 				distanceCache[index] = distanceMeasure.distance(patterns.get(x).data, patterns.get(y).data);
 			}
 		}
+		log.info("Done");
 	}
 
 	@Override
 	public double calculateDistance(int x, int y) {
 		if(x < 0 || y < 0)
 			throw new IllegalArgumentException("No pattern at (" + x + ", " + y + ")");
+
 		if(x == y)
 			return 0.0;
-		if(y > x)
+
+		if(y > x)	// swap the x and y index
 		{
 			int tmp = x;
 			x = y;
 			y = tmp;
 		}
-		int numPatterns = patterns.size();
-		return distanceCache[x + (numPatterns * y) - (((y + 1) * (y + 2)) / 2)];
+		return distanceCache[x + (patterns.size() * y) - (((y + 1) * (y + 2)) / 2)];
 	}
 
 	@Override
