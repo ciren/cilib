3e622b127d2ce0c971c7d337eda57cff1fb56ca3
Gary Pampara <gpampara@gmail.com>
2007-12-04 04:55:57 +0000

    r870@minifireslug:  gpampara | 2007-12-03 15:16:57 +0200  Removed some code that was implicitly specified in the Entity interface.
    git-svn-id: http://cilib.svn.sourceforge.net/svnroot/cilib/trunk@640 6919081d-ccfc-0310-b8b8-fc61df93b961

--- src/main/java/net/sourceforge/cilib/entity/AbstractEntity.java
+++ src/main/java/net/sourceforge/cilib/entity/AbstractEntity.java
@@ -1,33 +1,11 @@
 package net.sourceforge.cilib.entity;
 
-import net.sourceforge.cilib.problem.Fitness;
-import net.sourceforge.cilib.problem.OptimisationProblem;
 import net.sourceforge.cilib.type.types.Blackboard;
 import net.sourceforge.cilib.type.types.Type;
 
 public abstract class AbstractEntity implements Entity {
 	
 	protected Blackboard<String, Type> properties = new Blackboard<String, Type>();
-	
-	public abstract Entity getClone();
-
-	public abstract void calculateFitness();
-	
-	public abstract void calculateFitness(boolean count);
-	
-	public abstract int compareTo(Entity o);
-
-	public abstract Type getContents();
-
-	public abstract int getDimension();
-
-	public abstract Fitness getFitness();
-
-	public abstract void initialise(OptimisationProblem problem);
-
-	public abstract void reinitialise();
-	
-	public abstract void setContents(Type type);
 	
 	/**
 	 * Get the properties associate with the <code>Entity</code>
