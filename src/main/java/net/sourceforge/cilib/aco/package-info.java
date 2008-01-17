f739933d573d3bbadfe364268f216b4bc1659b91
Gary Pampara <gpampara@gmail.com>
2008-01-17 12:52:53 +0000

    r920@minifireslug:  gpampara | 2008-01-17 14:59:50 +0200  Added the initial package-info.java files for the JavaDoc documentation.
    git-svn-id: http://cilib.svn.sourceforge.net/svnroot/cilib/trunk@662 6919081d-ccfc-0310-b8b8-fc61df93b961

---
+++ src/main/java/net/sourceforge/cilib/aco/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package defines the framework extensions for Ant Colony Optimisation.
+ * 
+ * The remainder of this file needs to be completed.
+ */
+package net.sourceforge.cilib.aco;
