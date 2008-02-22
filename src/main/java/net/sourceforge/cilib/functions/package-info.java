ce861807b15eb19ce5196985f14d124a6d98c7c5
Gary Pampara <gpampara@gmail.com>
2008-02-22 06:50:27 +0000

    r933@minifireslug:  gpampara | 2008-02-22 08:56:09 +0200  Added some package level JavaDoc for the top level function package.
    git-svn-id: http://cilib.svn.sourceforge.net/svnroot/cilib/trunk@669 6919081d-ccfc-0310-b8b8-fc61df93b961

---
+++ src/main/java/net/sourceforge/cilib/functions/package-info.java
@@ -0,0 +1,24 @@
+/**
+ * <p>
+ * Collection of different types of functions. The function classes are categorized into
+ * different types:
+ * 
+ * <ul>
+ *   <li>Continuous functions</li>
+ *   <li>Discrete functions</li>
+ *   <li>Clustering functions</li>
+ * </ul>
+ * 
+ * <p>
+ * Each function provides details about itself within the JavaDoc. For example, continuous
+ * functions state the function properties such as:
+ * 
+ * <ul>
+ *   <li>Multi or Unimodal</li>
+ *   <li>Continuous or discontinuous</li>
+ *   <li>Separable or Non-separable</li>
+ *   <li>Input values for the the global minimum / maximum, if it is known.</li>
+ *   <li>The accepted search space domain of the function for benchmark purposes.</li>
+ * </ul>
+ */
+package net.sourceforge.cilib.functions;
