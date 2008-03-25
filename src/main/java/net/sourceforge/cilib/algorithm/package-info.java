73719a62b6ccfbbaa96203ad385bf46e0ea22e44
Gary Pampara <gpampara@gmail.com>
2008-03-25 05:33:49 +0000

    r985@minifireslug:  gpampara | 2008-03-24 12:31:31 +0200  Trailing characters removed.
    git-svn-id: http://cilib.svn.sourceforge.net/svnroot/cilib/trunk@697 6919081d-ccfc-0310-b8b8-fc61df93b961

--- src/main/java/net/sourceforge/cilib/algorithm/package-info.java
+++ src/main/java/net/sourceforge/cilib/algorithm/package-info.java
@@ -1,11 +1,11 @@
 /**
  * The <it>Algorithm</it> package provides the generic algorithm foundation
  * for all {@link Algorithm} instances.
- * 
+ *
  * All {@link Algorithm}s are defined to execute as a separate thread within
- * the Java VM. The the contained components within the algorithm class are 
- * plugable, resulting in each algorithm providing a generic skeleton that is 
- * filled in during construction of the algorithm instance. 
- * 
+ * the Java VM. The the contained components within the algorithm class are
+ * plugable, resulting in each algorithm providing a generic skeleton that is
+ * filled in during construction of the algorithm instance.
+ *
  */
 package net.sourceforge.cilib.algorithm;
