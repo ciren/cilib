91039158497ddb16b6df3a82260a774a9ac48f17
Gary Pampara <gpampara@gmail.com>
2008-03-25 05:34:52 +0000

    r987@minifireslug:  gpampara | 2008-03-24 23:24:07 +0200  Corrected a few JavaDocs.
    git-svn-id: http://cilib.svn.sourceforge.net/svnroot/cilib/trunk@699 6919081d-ccfc-0310-b8b8-fc61df93b961

--- src/main/java/net/sourceforge/cilib/algorithm/population/PopulationBasedAlgorithm.java
+++ src/main/java/net/sourceforge/cilib/algorithm/population/PopulationBasedAlgorithm.java
@@ -1,11 +1,11 @@
 /*
  * PopulationBasedAlgorithm.java
- * 
+ *
  * Created on Feb 8, 2006
  *
- * Copyright (C) 2003 - 2006 
+ * Copyright (C) 2003 - 2006
  * Computational Intelligence Research Group (CIRG@UP)
- * Department of Computer Science 
+ * Department of Computer Science
  * University of Pretoria
  * South Africa
  *
@@ -22,7 +22,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
  */
 package net.sourceforge.cilib.algorithm.population;
 
@@ -39,49 +38,62 @@ import net.sourceforge.cilib.entity.Topology;
 public abstract class PopulationBasedAlgorithm extends Algorithm {
 	protected PopulationInitialisationStrategy initialisationStrategy;
 
+	/**
+	 * Create an empty {@linkplain PopulationBasedAlgorithm}.
+	 */
 	public PopulationBasedAlgorithm() {
 	}
 
+	/**
+	 * Copy costructor. Duplicate the state of the current instance into
+	 * a new instance.
+	 * @param copy The {@linkplain PopulationBasedAlgorithm} to copy.
+	 */
 	public PopulationBasedAlgorithm(PopulationBasedAlgorithm copy) {
 		super(copy);
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	public abstract PopulationBasedAlgorithm getClone();
 
 	/**
-	 * Perform the iteration within the algorithm
+	 * Perform the iteration within the algorithm.
 	 */
 	protected abstract void algorithmIteration();
 
 	/**
 	 * Get the size of the current population within the algorithm.
-	 * @return The size of the current Population
+	 * @return The size of the current Population.
 	 */
 	public int getPopulationSize() {
 		return this.initialisationStrategy.getEntityNumber();
 	}
 
 	/**
-	 * Get the currently associated topology for the algorithm
-	 * @return The currently associated topology
+	 * Get the currently associated topology for the algorithm.
+	 * @return The currently associated topology.
 	 */
 	public abstract Topology<? extends Entity> getTopology();
 	
 	/**
-	 * Set the <tt>Topology</tt> for the population-based algorithm
-	 * @param topology The <tt>Topology</tt> to be set
+	 * Set the <tt>Topology</tt> for the population-based algorithm.
+	 * @param topology The {@linkplain Topology} to be set.
 	 */
 	public abstract void setTopology(Topology<? extends Entity> topology);
 
 	/**
-	 * @return
+	 * Get the currently set {@linkplain PopulationInitialisationStrategy}.
+	 * @return The current {@linkplain PopulationInitialisationStrategy}.
 	 */
 	public PopulationInitialisationStrategy getInitialisationStrategy() {
 		return initialisationStrategy;
 	}
 
 	/**
-	 * @param initialisationStrategy
+	 * Set the {@linkplain PopulationInitialisationStrategy} to be used.
+	 * @param initialisationStrategy The {@linkplain PopulationInitialisationStrategy} to use.
 	 */
 	public void setInitialisationStrategy(PopulationInitialisationStrategy initialisationStrategy) {
 		this.initialisationStrategy = initialisationStrategy;
