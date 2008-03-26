cbaf85e9e58b0e30dba207084bfeee7f253ecd3c
Gary Pampara <gpampara@gmail.com>
2008-03-26 05:55:51 +0000

    r1004@minifireslug:  gpampara | 2008-03-26 08:01:56 +0200  Cleanup around the CandidateSolution classes.
    git-svn-id: http://cilib.svn.sourceforge.net/svnroot/cilib/trunk@702 6919081d-ccfc-0310-b8b8-fc61df93b961

--- src/main/java/net/sourceforge/cilib/entity/CandidateSolution.java
+++ src/main/java/net/sourceforge/cilib/entity/CandidateSolution.java
@@ -1,3 +1,28 @@
+/*
+ * CandidateSolution.java
+ *
+ * Created on January 17, 2008, 4:54 PM
+ *
+ * Copyright (C) 2003 - 2006
+ * Computational Intelligence Research Group (CIRG@UP)
+ * Department of Computer Science
+ * University of Pretoria
+ * South Africa
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 package net.sourceforge.cilib.entity;
 
 import java.io.Serializable;
@@ -13,20 +38,20 @@ import net.sourceforge.cilib.util.Cloneable;
  * associated {@linkplain Fitness} value.
  */
 public interface CandidateSolution extends Serializable, Cloneable {
-	
+
 	/**
 	 * Get the contents of the <code>CandidateSoltion</code>. i.e.: The
 	 * potential solution.
 	 * @return A {@linkplain Type} representing the solution.
 	 */
 	public Type getContents();
-	
+
 	/**
 	 * Set the solution that the <code>CandidateSolution</code> represents.
 	 * @param contents The potential solution to set.
 	 */
 	public void setContents(Type contents);
-	
+
 	/**
 	 * Obtain the {@linkplain Fitness} of the current <code>CandidateSolution</code>.
 	 * @return The current {@linkplain Fitness} value.
--- src/main/java/net/sourceforge/cilib/entity/CandidateSolutionMixin.java
+++ src/main/java/net/sourceforge/cilib/entity/CandidateSolutionMixin.java
@@ -1,3 +1,28 @@
+/*
+ * CandidateSolutionMixin.java
+ *
+ * Created on January 21, 2007, 4:54 PM
+ *
+ * Copyright (C) 2003 - 2006
+ * Computational Intelligence Research Group (CIRG@UP)
+ * Department of Computer Science
+ * University of Pretoria
+ * South Africa
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 package net.sourceforge.cilib.entity;
 
 import net.sourceforge.cilib.problem.Fitness;
@@ -7,7 +32,7 @@ import net.sourceforge.cilib.type.types.Type;
 /**
  * <p>
  * A <code>CandidateSolution</code> is a potential solution within an optimisation.
- * 
+ *
  * <p>
  * <code>CandidateSolution</code> is a base class that all concrete <code>Entity</code>
  * instances inherit from. All <code>Entity</code> objects have their respective contents
@@ -17,33 +42,33 @@ import net.sourceforge.cilib.type.types.Type;
 public class CandidateSolutionMixin implements CandidateSolution {
 	private static final long serialVersionUID = 4539668687773346284L;
 	private final Blackboard<String, Type> properties;
-	
+
 	/**
 	 * Create the Mixin class, providing the reference to the shared {@linkplain Blackboard}
 	 * data structure.
 	 * @param properties The shared {@linkplain Blackboard}
 	 */
-	public CandidateSolutionMixin(Blackboard<String,Type> properties) {
+	public CandidateSolutionMixin(Blackboard<String, Type> properties) {
 		this.properties = properties;
 	}
-	
+
 	/**
 	 * Instantiate a new instance, based on the provided <code>CandidateSolutionMixin</code>.
-	 * This is a shallow copy instantiation. 
-	 * @param copy The template object ot copy.
+	 * This is a shallow copy instantiation.
+	 * @param copy The template object to copy.
 	 */
 	public CandidateSolutionMixin(CandidateSolutionMixin copy) {
 		this.properties = copy.properties;
 	}
-	
+
 	/**
-	 * Get a clone of the current {@linkplain CandidateSolutionMixin}
+	 * Get a clone of the current {@linkplain CandidateSolutionMixin}.
 	 * @return A shallow clone of the current object - The {@linkplain Blackboard} is not cloned.
 	 */
 	public CandidateSolutionMixin getClone() {
 		return new CandidateSolutionMixin(this);
 	}
-	
+
 	/**
 	 * Get the associated candidate solution representation that the current {@linkplain Entity}
 	 * represents.
@@ -52,7 +77,7 @@ public class CandidateSolutionMixin implements CandidateSolution {
 	public Type getContents() {
 		return properties.get("content");
 	}
-	
+
 	/**
 	 * {@inheritDoc}
 	 */
@@ -66,5 +91,5 @@ public class CandidateSolutionMixin implements CandidateSolution {
 	public Fitness getFitness() {
 		return (Fitness) properties.get("fitness");
 	}
-	
+
 }
