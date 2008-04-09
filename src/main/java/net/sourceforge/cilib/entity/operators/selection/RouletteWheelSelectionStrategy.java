42c56016cc3b0168ed0adcc7823495a5f2363141
Gary Pampara <gpampara@gmail.com>
2008-04-09 05:40:37 +0000

    r1037@minifireslug:  gpampara | 2008-04-09 07:48:46 +0200  Added a new selection strategy. Thanks to Vikash for the patch.
    git-svn-id: http://cilib.svn.sourceforge.net/svnroot/cilib/trunk@718 6919081d-ccfc-0310-b8b8-fc61df93b961

---
+++ src/main/java/net/sourceforge/cilib/entity/operators/selection/RouletteWheelSelectionStrategy.java
@@ -0,0 +1,137 @@
+/*
+ * RouletteWheelSelectionStrategy.java
+ *
+ * Copyright (C) 2003 - 2008
+ * Computational Intelligence Research Group (CIRG@UP)
+ * Department of Computer Science
+ * University of Pretoria
+ * South Africa
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package net.sourceforge.cilib.entity.operators.selection;
+
+import net.sourceforge.cilib.entity.Entity;
+import net.sourceforge.cilib.entity.Topology;
+import net.sourceforge.cilib.math.random.generator.MersenneTwister;
+import net.sourceforge.cilib.math.random.generator.Random;
+import net.sourceforge.cilib.problem.InferiorFitness;
+
+/**
+ * This class implements Roulette Wheel selection, also known as proportionate
+ * selection. The probability of selecting an {@linkplain Entity} is equal to
+ * the ratio of its fitness to the sum total of the fitness of all the
+ * {@linkplain Entity}s in the population.
+ * 
+ * <p>
+ * This method of selection is not particularly useful if a single solution has
+ * very high fitness as compared to the rest. This results in selecting the
+ * fittest solution every time we make a selection.
+ * 
+ * @author Vikash Ranjan Parida
+ */
+public class RouletteWheelSelectionStrategy extends SelectionStrategy {
+	private Random random;
+
+	/**
+	 * Create an instance of the {@linkplain RouletteWheelSelectionStrategy}.
+	 */
+	public RouletteWheelSelectionStrategy() {
+		this.random = new MersenneTwister();
+	}
+
+	/**
+	 * Create a copy of the provided instance.
+	 * @param copy The instance to copy.
+	 */
+	public RouletteWheelSelectionStrategy(final RouletteWheelSelectionStrategy copy) {
+		this.random = copy.random.getClone();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public SelectionStrategy getClone() {
+		return new RouletteWheelSelectionStrategy(this);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public Entity select(final Topology<? extends Entity> population) {
+		double totalFitness = getTotalFitness(population);
+		double cumulativeProb = 0;
+		double valueToPick = random.nextDouble();
+
+		// If the fitness of all the Entities is zero, we randomly select one.
+		if (totalFitness == InferiorFitness.instance().getValue()) {
+			return population.get(random.nextInt(population.size()));
+		}
+
+		for (Entity entity : population) {
+			double probability = entity.getFitness().getValue() / totalFitness;
+			if (valueToPick < cumulativeProb + probability) {
+				return entity;
+			}
+
+			cumulativeProb += probability;
+		}
+
+		return population.get(population.size() - 1);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void performOperation(Topology<? extends Entity> topology, Topology<Entity> offspring) {
+		offspring.add(select(topology));
+	}
+
+	/**
+	 * It calculates the sum total of the fitness of all the {@linkplain Entity}s
+	 * in the {@linkplain Topology}.
+	 * 
+	 * @param topology The {@linkplain Topology} to use.
+	 * @return The sum total of the fitness values.
+	 */
+	private double getTotalFitness(final Topology<? extends Entity> topology) {
+		double totalFitness = 0;
+		for (Entity entity : topology) {
+			totalFitness += entity.getFitness().getValue();
+		}
+
+		return totalFitness;
+	}
+
+	/**
+	 * Get the current {@linkplain Random} number generator. 
+	 * @return The current {@linkplain Random}.
+	 */
+	public Random getRandom() {
+		return random;
+	}
+
+	/**
+	 * Set the {@linkplain Random} number generator.
+	 * @param random the value to set.
+	 */
+	public void setRandom(Random random) {
+		this.random = random;
+	}
+
+}
