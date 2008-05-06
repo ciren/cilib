a376df614b47c070ed0bbafbca0883b92c44c0f6
Gary Pampara <gpampara@gmail.com>
2008-05-06 12:22:43 +0000

    r1186@minifireslug:  gpampara | 2008-05-06 16:23:06 +0200  Header fixes and javadoc additions.
    git-svn-id: http://cilib.svn.sourceforge.net/svnroot/cilib/trunk@795 6919081d-ccfc-0310-b8b8-fc61df93b961

--- src/main/java/net/sourceforge/cilib/xml/XMLAlgorithmFactory.java
+++ src/main/java/net/sourceforge/cilib/xml/XMLAlgorithmFactory.java
@@ -1,12 +1,7 @@
 /*
- * XMLAlgorithmFactory.java
- *
- * Created on June 5, 2003, 3:21 PM
- *
- * 
- * Copyright (C) 2003 - 2006 
+ * Copyright (C) 2003 - 2008
  * Computational Intelligence Research Group (CIRG@UP)
- * Department of Computer Science 
+ * Department of Computer Science
  * University of Pretoria
  * South Africa
  *
@@ -22,10 +17,8 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA 
- *   
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
-
 package net.sourceforge.cilib.xml;
 
 import net.sourceforge.cilib.algorithm.Algorithm;
@@ -44,7 +37,7 @@ public class XMLAlgorithmFactory extends XMLObjectFactory implements AlgorithmFa
 		super(null);
 	}
 	
-    /** Creates a new instance of XMLAlgorithmFactory */
+    /** Creates a new instance of XMLAlgorithmFactory. */
     public XMLAlgorithmFactory(Document xmlDocument, Element xmlAlgorithmDescription) {
         super(xmlDocument, xmlAlgorithmDescription);
         if (!xmlAlgorithmDescription.getTagName().equals("algorithm")) {
--- src/main/java/net/sourceforge/cilib/xml/XMLMeasurementFactory.java
+++ src/main/java/net/sourceforge/cilib/xml/XMLMeasurementFactory.java
@@ -1,12 +1,7 @@
 /*
- * XMLMeasurementFactory.java
- *
- * Created on July 2, 2003, 3:21 PM
- *
- * 
- * Copyright (C) 2003 - 2006 
+ * Copyright (C) 2003 - 2008
  * Computational Intelligence Research Group (CIRG@UP)
- * Department of Computer Science 
+ * Department of Computer Science
  * University of Pretoria
  * South Africa
  *
@@ -22,10 +17,8 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA 
- *   
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
-
 package net.sourceforge.cilib.xml;
 
 import net.sourceforge.cilib.measurement.Measurement;
@@ -44,7 +37,7 @@ public class XMLMeasurementFactory extends XMLObjectFactory implements Measureme
 		super(null);
 	}
 	
-    /** Creates a new instance of XMLMeasurementFactory */
+    /** Creates a new instance of XMLMeasurementFactory. */
     public XMLMeasurementFactory(Document xmlDocument, Element xmlMeasurementDescription) {
         super(xmlDocument, xmlMeasurementDescription);
         if (!xmlMeasurementDescription.getTagName().equals("measurement")) {
