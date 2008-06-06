3c772f2e73ed8b5ffecc289c99a94d7a193298f7
Gary Pampara <gpampara@gmail.com>
2008-06-06 04:12:51 +0000

    r1280@minifireslug:  gpampara | 2008-06-06 07:53:40 +0200  Added the refactored tests and cleaned up the MathUtilTest.
    git-svn-id: http://cilib.svn.sourceforge.net/svnroot/cilib/trunk@839 6919081d-ccfc-0310-b8b8-fc61df93b961

---
+++ src/test/java/net/sourceforge/cilib/functions/activation/SigmoidTest.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2003 - 2008
+ * Computational Intelligence Research Group (CIRG@UP)
+ * Department of Computer Science
+ * University of Pretoria
+ * South Africa
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package net.sourceforge.cilib.functions.activation;
+
+import static org.junit.Assert.assertEquals;
+import net.sourceforge.cilib.controlparameter.ConstantControlParameter;
+
+import org.junit.Test;
+
+public class SigmoidTest {
+
+	@Test
+	public void evaluate() {
+		Sigmoid sigmoid = new Sigmoid();
+		assertEquals(0.5, sigmoid.evaluate(Double.valueOf(0.0)), 0);
+		assertEquals(1.0, sigmoid.evaluate(Double.POSITIVE_INFINITY), 0);
+		assertEquals(1.0, sigmoid.evaluate(Double.MAX_VALUE), 0);
+		assertEquals(0.0, sigmoid.evaluate(-Double.MAX_VALUE), 0);
+		assertEquals(0.0, sigmoid.evaluate(Double.NEGATIVE_INFINITY), 0);
+		assertEquals(0.5, sigmoid.evaluate(Double.MIN_VALUE), 0);
+	}
+	
+	@Test(expected = UnsupportedOperationException.class)
+	public void invalidEvaluate() {
+		Sigmoid sigmoid = new Sigmoid();
+		sigmoid.setSteepness(new ConstantControlParameter(-8.0));
+		
+		sigmoid.evaluate(0.0);
+	}
+}
--- src/test/java/net/sourceforge/cilib/math/MathUtilTest.java
+++ src/test/java/net/sourceforge/cilib/math/MathUtilTest.java
@@ -85,19 +85,4 @@ public class MathUtilTest {
 		assertEquals(1.0, MathUtil.combination(1, 1), Double.MIN_NORMAL);
 	}
 	
-	@Test
-	public void sigmoid() {
-		assertEquals(0.5, MathUtil.sigmoid(0.0), 0);
-		assertEquals(1.0, MathUtil.sigmoid(Double.POSITIVE_INFINITY), 0);
-		assertEquals(1.0, MathUtil.sigmoid(Double.MAX_VALUE), 0);
-		assertEquals(0.0, MathUtil.sigmoid(-Double.MAX_VALUE), 0);
-		assertEquals(0.0, MathUtil.sigmoid(Double.NEGATIVE_INFINITY), 0);
-		assertEquals(0.5, MathUtil.sigmoid(Double.MIN_VALUE), 0);
-	}
-	
-	@Test(expected = UnsupportedOperationException.class)
-	public void invalidSigmoid() {
-		MathUtil.sigmoid(-1.0, 0.0, 0.0);
-	}
-
 }
