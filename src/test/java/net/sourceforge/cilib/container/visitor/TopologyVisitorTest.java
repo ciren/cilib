8047af8fcd46d94ac971fbf03a5b4c61ae787618
Gary Pampara <gpampara@gmail.com>
2008-09-02 07:41:14 +0000

    Simplified the unit test, I think it's valid...
    git-svn-id: http://cilib.svn.sourceforge.net/svnroot/cilib/trunk@922 6919081d-ccfc-0310-b8b8-fc61df93b961

--- src/test/java/net/sourceforge/cilib/container/visitor/TopologyVisitorTest.java
+++ src/test/java/net/sourceforge/cilib/container/visitor/TopologyVisitorTest.java
@@ -1,41 +1,60 @@
+/*
+ * Copyright (C) 2003 - 2008
+ * Computational Intelligence Research Group (CIRG@UP)
+ * Department of Computer Science
+ * University of Pretoria
+ * South Africa
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 package net.sourceforge.cilib.container.visitor;
 
 import static org.junit.Assert.assertTrue;
-import net.sourceforge.cilib.algorithm.initialisation.ClonedPopulationInitialisationStrategy;
-import net.sourceforge.cilib.algorithm.initialisation.PopulationInitialisationStrategy;
+import net.sourceforge.cilib.entity.Particle;
+import net.sourceforge.cilib.entity.Topology;
 import net.sourceforge.cilib.entity.visitor.RadiusVisitor;
 import net.sourceforge.cilib.entity.visitor.TopologyVisitor;
-import net.sourceforge.cilib.functions.continuous.Spherical;
-import net.sourceforge.cilib.problem.FunctionMinimisationProblem;
 import net.sourceforge.cilib.pso.PSO;
-import net.sourceforge.cilib.pso.particle.StandardParticle;
-import net.sourceforge.cilib.stoppingcondition.MaximumIterations;
 
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.jmock.integration.junit4.JUnit4Mockery;
+import org.jmock.lib.legacy.ClassImposteriser;
 import org.junit.Test;
 
 public class TopologyVisitorTest {
-
+	private Mockery context = new JUnit4Mockery() {{
+		setImposteriser(ClassImposteriser.INSTANCE);
+	}};
+	
 	/**
-	 * Initialise a dummy algorithm and then test if the visitor knows
+	 * Initialize a dummy algorithm and then test if the visitor knows
 	 * which algorithm it used for it's evaluation.
-	 * 
-	 * @TODO: This setup is clumsy.... better manner to do this?
 	 */
+	@SuppressWarnings("unchecked")
 	@Test
 	public void currentAlgorithmUsed() {
+		final Topology<Particle> topology = context.mock(Topology.class);
+		final TopologyVisitor visitor = new RadiusVisitor();
+		
 		PSO pso = new PSO();
-		PopulationInitialisationStrategy strategy = new ClonedPopulationInitialisationStrategy();
-		strategy.setEntityNumber(2);
-		strategy.setEntityType(new StandardParticle());
-		pso.setInitialisationStrategy(strategy);
-		pso.addStoppingCondition(new MaximumIterations(1000));
+		pso.setTopology(topology);
 		
-		FunctionMinimisationProblem problem = new FunctionMinimisationProblem();
-		problem.setFunction(new Spherical());
-		pso.setOptimisationProblem(problem);
-		
-		pso.initialise();
-		TopologyVisitor visitor = new RadiusVisitor();
+		context.checking(new Expectations() {{
+			one(topology).accept(visitor);
+		}});
 		
 		pso.accept(visitor);
 		
