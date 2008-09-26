eff761acf9ec4d1f737d5fe184b12e79f4b533e6
Gary Pampara <gpampara@gmail.com>
2008-09-26 10:29:05 +0000

    r953@compaq:  gpampara | 2008-09-26 10:28:49 +0000  Added Javadoc to avoid any confusion with the problem change strategies.
    git-svn-id: http://cilib.svn.sourceforge.net/svnroot/cilib/trunk@944 6919081d-ccfc-0310-b8b8-fc61df93b961

--- src/main/java/net/sourceforge/cilib/problem/changestrategy/ChangeStrategy.java
+++ src/main/java/net/sourceforge/cilib/problem/changestrategy/ChangeStrategy.java
@@ -2,8 +2,22 @@ package net.sourceforge.cilib.problem.changestrategy;
 
 import net.sourceforge.cilib.problem.Problem;
 
+/**
+ * Interface detailing the potential operations that may be applied to the current problem.
+ * 
+ * This is particularly useful in Dynamic Environments when the need to alter the
+ * problem search space.
+ * 
+ * It is possible to classify all problem instances as being dynamic problems. Problems that
+ * remain unchanged are effectively problems where the applied change is a change that
+ * preserves the problem search space, thus leaving it unchanged.
+ */
 public interface ChangeStrategy {
 
+	/**
+	 * Perform a change on the provided {@code problem} instance.
+	 * @param problem The problem on which a change is to be applied.
+	 */
 	void change(Problem problem);
 
 }
--- src/main/java/net/sourceforge/cilib/problem/changestrategy/NoChangeStrategy.java
+++ src/main/java/net/sourceforge/cilib/problem/changestrategy/NoChangeStrategy.java
@@ -2,8 +2,17 @@ package net.sourceforge.cilib.problem.changestrategy;
 
 import net.sourceforge.cilib.problem.Problem;
 
+/**
+ * Environmental change strategy for problems that does nothing.
+ */
 public class NoChangeStrategy implements ChangeStrategy {
 
+	/**
+	 * Do not change the search space of the provided problem instance. Leave
+	 * the search space intact.
+	 * <p>
+	 * This method simply returns and performs no actions.
+	 */
 	public void change(Problem problem) {
 		// This is intentionally left empty
 		// For Theuns: Living Laxly Like A Lazy Lounge Lizzard
