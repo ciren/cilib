841dbf8246f798fa68c1d7e9f68e285f5f3a8a79
Gary Pampara <gpampara@gmail.com>
2008-10-02 07:23:54 +0000

    r963@compaq:  gpampara | 2008-10-02 07:12:46 +0000  Added missing license information for all files.
    git-svn-id: http://cilib.svn.sourceforge.net/svnroot/cilib/trunk@949 6919081d-ccfc-0310-b8b8-fc61df93b961

--- src/test/java/net/sourceforge/cilib/container/BitArrayTest.java
+++ src/test/java/net/sourceforge/cilib/container/BitArrayTest.java
@@ -1,30 +1,23 @@
-/*
- * BitArrayTest.java
- * JUnit based test
- *
- * Created on January 04, 2005, 4:45 PM
- *
- * 
- * Copyright (C) 2003 - 2006 
+/**
+ * Copyright (C) 2003 - 2008
  * Computational Intelligence Research Group (CIRG@UP)
- * Department of Computer Science 
+ * Department of Computer Science
  * University of Pretoria
  * South Africa
- *
+ * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA 
- *   
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
 package net.sourceforge.cilib.container;
--- src/test/java/net/sourceforge/cilib/measurement/multiple/MultipleFitnessTest.java
+++ src/test/java/net/sourceforge/cilib/measurement/multiple/MultipleFitnessTest.java
@@ -1,29 +1,25 @@
-/*
- * MultipleFitnessTest.java
- * 
- * Created on Oct 25, 2005
- *
- * Copyright (C) 2003 - 2006 
+/**
+ * Copyright (C) 2003 - 2008
  * Computational Intelligence Research Group (CIRG@UP)
- * Department of Computer Science 
+ * Department of Computer Science
  * University of Pretoria
  * South Africa
- *
+ * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
  */
+
 package net.sourceforge.cilib.measurement.multiple;
 
 import static org.junit.Assert.assertEquals;
