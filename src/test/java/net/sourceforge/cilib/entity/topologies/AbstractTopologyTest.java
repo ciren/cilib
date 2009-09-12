7000930c7c0a4c04533d16b40f73616a81ff1de0
Gary Pampara <gpampara@gmail.com>
2009-09-12 11:05:11 +0200

    Remove useless tests.
    The tests maintained by the AsctractTopologyTest where a little flaky.
    As a result these tests should be removed.
    
    Signed-off-by: Gary Pampara <gpampara@gmail.com>

--- src/test/java/net/sourceforge/cilib/entity/topologies/AbstractTopologyTest.java
+++ src/test/java/net/sourceforge/cilib/entity/topologies/AbstractTopologyTest.java
@@ -21,16 +21,9 @@
  */
 package net.sourceforge.cilib.entity.topologies;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 import static org.hamcrest.CoreMatchers.is;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.List;
 
-import net.sourceforge.cilib.ec.Individual;
-import net.sourceforge.cilib.entity.Entity;
 import net.sourceforge.cilib.entity.EntityType;
 import net.sourceforge.cilib.entity.Particle;
 import net.sourceforge.cilib.entity.Topology;
@@ -39,17 +32,22 @@ import net.sourceforge.cilib.entity.comparator.DescendingFitnessComparator;
 import net.sourceforge.cilib.entity.comparator.SocialBestFitnessComparator;
 import net.sourceforge.cilib.problem.MaximisationFitness;
 import net.sourceforge.cilib.problem.MinimisationFitness;
-import net.sourceforge.cilib.pso.PSO;
 
 import net.sourceforge.cilib.pso.particle.StandardParticle;
+import org.jmock.Mockery;
+import org.jmock.integration.junit4.JMock;
+import org.jmock.integration.junit4.JUnit4Mockery;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 /**
  *
  * @author Gary Pampara
  */
+@RunWith(JMock.class)
 public class AbstractTopologyTest {
+    private Mockery context = new JUnit4Mockery();
 
     @Test
     public void comparatorBestEntity() {
@@ -98,57 +96,6 @@ public class AbstractTopologyTest {
         Particle best = topology.getBestEntity();
 
         Assert.assertThat(best, is(p1));
-    }
-
-
-    /**
-     * Test the setter method for the IoC container
-     *
-     */
-    @Test
-    public void testIoCSetterMethod() {
-
-        try {
-            PSO p = new PSO();
-
-            Method m = p.getClass().getMethod("setTopology", new Class[] {Topology.class});
-
-            Topology<Individual> top1 = new LBestTopology<Individual>();
-            Topology<Particle> top2 = new VonNeumannTopology<Particle>();
-            Topology<Entity> top3 = new HypercubeTopology<Entity>();
-
-            m.invoke(p, top1);
-            assertTrue(p.getTopology() instanceof LBestTopology);
-
-            m.invoke(p, top2);
-            assertTrue(p.getTopology() instanceof VonNeumannTopology);
-
-            m.invoke(p, top3);
-            assertTrue(p.getTopology() instanceof HypercubeTopology);
-
-        } catch (SecurityException e) {
-            e.printStackTrace();
-        } catch (NoSuchMethodException e) {
-            e.printStackTrace();
-        } catch (IllegalArgumentException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        } catch (InvocationTargetException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Test
-    public void testGeneric() {
-        PSO p = new PSO();
-        List<Particle> list = p.getTopology().asList();
-
-        assertEquals(p.getTopology().size(), list.size());
-
-        for (Particle particle : p.getTopology()) {
-            assertTrue(list.contains(particle));
-        }
     }
 
 }
