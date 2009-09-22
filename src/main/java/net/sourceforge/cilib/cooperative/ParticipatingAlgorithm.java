bdc29a7a82501617e5682a0f8c849c58ecac500b
Gary Pampara <gpampara@gmail.com>
2009-09-22 12:57:59 +0200

    Corrected Participating algorithm javadoc.
    The javadoc regarding how this interface was used was not very clear.
    Reworded the javadocs to try make the interface intention clearer.
    
    Signed-off-by: Gary Pampara <gpampara@gmail.com>

--- src/main/java/net/sourceforge/cilib/cooperative/ParticipatingAlgorithm.java
+++ src/main/java/net/sourceforge/cilib/cooperative/ParticipatingAlgorithm.java
@@ -25,9 +25,10 @@ import net.sourceforge.cilib.entity.Entity;
 import net.sourceforge.cilib.problem.Fitness;
 
 /**
- * Any algorithm that implements this interface can be used as a participant in any Cooperative
- * Algorithm. Such Cooperative Algorithms should preferably inherit from
- * {@link net.sourceforge.cilib.algorithm.population.MultiPopulationBasedAlgorithm}. <b>Note:</b>
+ * Any algorithm that implements this interface can be used as a participating algorithm
+ * within a Cooperative Algorithm.
+ * <p>
+ * <b>Note:</b>
  * Currently this interface only supports continuous domained problems.
  * @author Edwin Peer
  * @author Theuns Cloete
@@ -35,18 +36,24 @@ import net.sourceforge.cilib.problem.Fitness;
 public interface ParticipatingAlgorithm {
     /**
      * Returns contribution to the solution for the co-operative optimisation algorithm.
-     * @return The algorithm's solution contribution.
+     * The contribution is defined as the most fit entity within the current participating
+     * algorithm.
+     * @return The algorithm's solution contribution, also known as the current best solution
+     *         from the algorithm.
      */
     public Entity getContribution();
 
     /**
-     * Returns the fitness of contribution to the solution.
-     * @return The fitness of the solution contribution.
+     * Obtain the fitness value of the best ssolution within the current sub-swarm of
+     * this participating algorithm within a co-operative algorithm.
+     * @return The fitness of the solution contribution for the current participating
+     *         algorithm.
      */
     public Fitness getContributionFitness();
 
     /**
-     * Updates the new fitness for the solution contribution.
+     * Updates the new fitness for the solution contribution of the current participating
+     * algorithm for the cooperative algorithm maintaining the current algorithm.
      * @param fitness The new fitness of the contribution.
      */
     public void updateContributionFitness(Fitness fitness);
