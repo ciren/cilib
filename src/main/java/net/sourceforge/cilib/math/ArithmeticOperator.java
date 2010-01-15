7d657329e22e64a0015776b702194a35bc650b32
Gary Pampara <gpampara@gmail.com>
2010-01-15 10:37:12 +0200

    Remove MutationOperatorStrategy
    The MutationOperatorStrategy is redundant and can be simply replaced by
    the ArithmeticOperator.
    
    Signed-off-by: Gary Pampara <gpampara@gmail.com>

---
+++ src/main/java/net/sourceforge/cilib/math/ArithmeticOperator.java
@@ -0,0 +1,66 @@
+/**
+ * Copyright (C) 2003 - 2009
+ * Computational Intelligence Research Group (CIRG@UP)
+ * Department of Computer Science
+ * University of Pretoria
+ * South Africa
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package net.sourceforge.cilib.math;
+
+/**
+ * Basic arithmetic operations. This class provides the simple operators so that
+ * they may be reused in other classes.
+ *
+ * @author gpampara
+ */
+public enum ArithmeticOperator {
+    ADDITION {
+        @Override
+        public <T extends Number> double evaluate(T left, T right) {
+            return left.doubleValue() + right.doubleValue();
+        }
+    },
+    SUBTRACTION {
+        @Override
+        public <T extends Number> double evaluate(T left, T right) {
+            return left.doubleValue() - right.doubleValue();
+        }
+    },
+    MULTIPLICATION {
+        @Override
+        public <T extends Number> double evaluate(T left, T right) {
+            return left.doubleValue() * right.doubleValue();
+        }
+    },
+    DEVISION {
+        @Override
+        public <T extends Number> double evaluate(T left, T right) {
+            return left.doubleValue() / right.doubleValue();
+        }
+    };
+
+    /**
+     * Evaluate the binary operator, given the left and the right values.
+     * @param <T> The {@code Number} type.
+     * @param left The left operand.
+     * @param right The right operand.
+     * @return A double representing the result.
+     */
+    public abstract <T extends Number> double evaluate(T left, T right);
+
+}
