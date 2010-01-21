b572672e31e8097bfb0eea2ca878d4ab73c4a9f1
Wiehann Matthysen <wiehann.matthysen@rorotika.com>
2010-01-21 12:26:31 +0200

    Proportional Ordering Patch
    Fixed the test by making sure that the ordering conforms to the way in
    which the random numbers are generated now.
    
    Signed-off-by: Wiehann Matthysen <wcmatthysen@gmail.com>
    Signed-off-by: Gary Pampara <gpampara@gmail.com>

--- src/test/java/net/sourceforge/cilib/util/selection/ordering/ProportionalOrderingTest.java
+++ src/test/java/net/sourceforge/cilib/util/selection/ordering/ProportionalOrderingTest.java
@@ -47,55 +47,56 @@ public class ProportionalOrderingTest {
         Assert.assertThat(ordered, is(true));
 
         // Random numbers in the range [0,1) will be generated in the following order:
-        // 0.730967787376657
-        // 0.24053641567148587
-        // 0.6374174253501083
-        // 0.5504370051176339
-        // 0.5975452777972018
-        // 0.3332183994766498
-        // 0.3851891847407185
-        // 0.984841540199809
-        // 0.8791825178724801
+
+        // 0.8147236874025613
+        // 0.13547700503158466
+        // 0.9057919358463374
+        // 0.8350085784045876
+        // 0.12698681606155282
+        // 0.9688677774872758
+        // 0.9133758577858514
+        // 0.2210340445095188
+        // 0.6323592410708524
 
         // Initial total sum is 45.
-        // First marker at: 45 * 0.730967787376657 = 32.8935504316 -> 8
-        // Order: (8|, 1, 2, 3, 4, 5, 6, 7, 9)
-        Assert.assertThat(entries.get(8).getElement(), is(8));
+        // First marker at: 45 * 0.8147236874025613 = 36.662565933 -> 9
+        // Order: (9|, 1, 2, 3, 4, 5, 6, 7, 8)
+        Assert.assertThat(entries.get(8).getElement(), is(9));
 
-        // Total sum is 45 - 8 = 37.
-        // First marker at: 37 * 0.24053641567148587 = 8.89984737979 -> 4
-        // Order: (8, 4|, 1, 2, 3, 5, 6, 7, 9)
-        Assert.assertThat(entries.get(7).getElement(), is(4));
+        // Total sum is 45 - 9 = 36.
+        // First marker at: 36 * 0.13547700503158466 = 4.87717218108 -> 3
+        // Order: (9, 3|, 1, 2, 4, 5, 6, 7, 8)
+        Assert.assertThat(entries.get(7).getElement(), is(3));
 
-        // Total sum is 37 - 4 = 33.
-        // First marker at: 33 * 0.6374174253501083 = 21.0347750366 -> 7
-        // Order: (8, 4, 7|, 1, 2, 3, 5, 6, 9)
-        Assert.assertThat(entries.get(6).getElement(), is(7));
+        // Total sum is 36 - 3 = 33.
+        // First marker at: 33 * 0.9057919358463374 = 29.8911338827 -> 8
+        // Order: (9, 3, 8|, 1, 2, 4, 5, 6, 7)
+        Assert.assertThat(entries.get(6).getElement(), is(8));
 
-        // Total sum is 33 - 7 = 26.
-        // First marker at: 26 * 0.5504370051176339 = 14.3113621329 -> 6
-        // Order: (8, 4, 7, 6|, 1, 2, 3, 5, 9)
-        Assert.assertThat(entries.get(5).getElement(), is(6));
+        // Total sum is 33 - 8 = 25.
+        // First marker at: 25 * 0.8350085784045876 = 20.87521446 -> 7
+        // Order: (9, 3, 8, 7|, 1, 2, 4, 5, 6)
+        Assert.assertThat(entries.get(5).getElement(), is(7));
 
-        // Total sum is 26 - 6 = 20.
-        // First marker at: 20 * 0.5975452777972018 = 11.9509055558 -> 9
-        // Order: (8, 4, 7, 6, 9|, 1, 2, 3, 5)
-        Assert.assertThat(entries.get(4).getElement(), is(9));
+        // Total sum is 25 - 7 = 18.
+        // First marker at: 18 * 0.12698681606155282 = 2.28576268908 -> 2
+        // Order: (9, 3, 8, 7, 2|, 1, 4, 5, 6)
+        Assert.assertThat(entries.get(4).getElement(), is(2));
 
-        // Total sum is 20 - 9 = 11.
-        // First marker at: 11 * 0.3332183994766498 = 3.66540239417 -> 3
-        // Order: (8, 4, 7, 6, 9, 3|, 1, 2, 5)
-        Assert.assertThat(entries.get(3).getElement(), is(3));
+        // Total sum is 18 - 2 = 16.
+        // First marker at: 16 * 0.9688677774872758 = 15.50188444 -> 6
+        // Order: (9, 3, 8, 7, 2, 6|, 1, 4, 5)
+        Assert.assertThat(entries.get(3).getElement(), is(6));
 
-        // Total sum is 11 - 3 = 8.
-        // First marker at: 8 * 0.3851891847407185 = 3.08151347792 -> 5
-        // Order: (8, 4, 7, 6, 9, 3, 5|, 1, 2)
+        // Total sum is 16 - 6 = 10.
+        // First marker at: 10 * 0.9133758577858514 = 9.1337585778 -> 5
+        // Order: (9, 3, 8, 7, 2, 6, 5|, 1, 4)
         Assert.assertThat(entries.get(2).getElement(), is(5));
 
-        // Total sum is 8 - 5 = 3.
-        // First marker at: 3 * 0.984841540199809 = 2.95452462057 -> 2
-        // Order: (8, 4, 7, 6, 9, 3, 5, 2|, 1)
-        Assert.assertThat(entries.get(1).getElement(), is(2));
+        // Total sum is 10 - 5 = 5.
+        // First marker at: 5 * 0.984841540199809 = 1.1051702225 -> 4
+        // Order: (9, 3, 8, 7, 2, 6, 5, 4|, 1)
+        Assert.assertThat(entries.get(1).getElement(), is(4));
 
         // Finally...
         Assert.assertThat(entries.get(0).getElement(), is(1));
@@ -141,9 +142,7 @@ public class ProportionalOrderingTest {
 
         @Override
         public double nextDouble() {
-            double result = this.randomProvider.nextDouble();
-            System.out.println("result : " + result);
-            return result;
+            return this.randomProvider.nextDouble();
         }
 
         @Override
