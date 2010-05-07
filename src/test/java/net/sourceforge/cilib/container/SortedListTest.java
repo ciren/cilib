a4c5523f6efb28d8476f04e3071e79f58406113e
Gary Pampara <gpampara@gmail.com>
2010-05-07 12:48:00 +0200

    Remove Pair class.
    The Pair class was a general container for two objects. This was rather
    handy, but it did not provide a clear purpose of what it was being used
    for. Seeing as that it was only used in very select areas, special
    containers have been created privately to achieve the same result, with
    better context of their intention.
    
    Signed-off-by: Gary Pampara <gpampara@gmail.com>

--- src/test/java/net/sourceforge/cilib/container/SortedListTest.java
+++ src/test/java/net/sourceforge/cilib/container/SortedListTest.java
@@ -24,10 +24,8 @@ package net.sourceforge.cilib.container;
 import java.util.ArrayList;
 import static org.junit.Assert.assertEquals;
 
-import java.util.Comparator;
 
 import java.util.List;
-import net.sourceforge.cilib.entity.Entity;
 import net.sourceforge.cilib.type.types.Int;
 
 import org.junit.Test;
@@ -45,30 +43,6 @@ public class SortedListTest {
         assertEquals(2, intList.get(0).getInt());
         assertEquals(5, intList.get(1).getInt());
         assertEquals(8, intList.get(2).getInt());
-    }
-
-
-    @Test
-    public void sortedPairAddition() {
-        SortedList<Pair<Integer, Entity>> pairList = new SortedList<Pair<Integer, Entity>>(new PairComparator());
-
-        Pair<Integer, Entity> p1 = new Pair<Integer, Entity>(3, null);
-        Pair<Integer, Entity> p3 = new Pair<Integer, Entity>(99, null);
-        Pair<Integer, Entity> p4 = new Pair<Integer, Entity>(-9, null);
-        Pair<Integer, Entity> p2 = new Pair<Integer, Entity>(0, null);
-        Pair<Integer, Entity> p5 = new Pair<Integer, Entity>(0, null);
-
-        pairList.add(p1);
-        pairList.add(p2);
-        pairList.add(p3);
-        pairList.add(p4);
-        pairList.add(p5);
-
-        assertEquals(-9, (int) pairList.get(0).getKey());
-        assertEquals(0,  (int) pairList.get(1).getKey());
-        assertEquals(0,  (int) pairList.get(2).getKey());
-        assertEquals(3,  (int) pairList.get(3).getKey());
-        assertEquals(99, (int) pairList.get(4).getKey());
     }
 
     /**
@@ -104,7 +78,7 @@ public class SortedListTest {
         v.add(new Int(1));
         v.add(new Int(50));
 
-        list.addAll(list.size()-1, v);
+        list.addAll(list.size() - 1, v);
 
         assertEquals(0, list.get(0).getInt());
         assertEquals(1, list.get(1).getInt());
@@ -112,13 +86,4 @@ public class SortedListTest {
         assertEquals(50, list.get(3).getInt());
         assertEquals(200, list.get(4).getInt());
     }
-
-
-    private class PairComparator implements Comparator<Pair<Integer, Entity>> {
-        @Override
-        public int compare(Pair<Integer, Entity> o1, Pair<Integer, Entity> o2) {
-            return o1.getKey().compareTo(o2.getKey());
-        }
-    }
-
 }
