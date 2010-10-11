e139e38c0bae31898439c1c6ed538cf94fdc43d8
Wiehann Matthysen <wcmatthysen@gmail.com>
2010-10-11 03:47:02 +0200

    Expanded on the Solution Distribution Test.
    Added an additional test for the SolutionDistribution meausurement to
    test the bi-solution case. Thus, for 2 solutions the solution
    distribution will always be 0 irrespective of the spacing between these
    solutions. This means that an MOO needs to find more than 2 solutions
    for the measurement to make any sense.

--- src/test/java/net/sourceforge/cilib/measurement/single/moo/SolutionDistributionTest.java
+++ src/test/java/net/sourceforge/cilib/measurement/single/moo/SolutionDistributionTest.java
@@ -48,20 +48,14 @@ public class SolutionDistributionTest {
     private Mockery mockery = new JUnit4Mockery();
 
     @Test
-    public void resultsRandomDistribution() {
+    public void testBiDistribution() {
         Algorithm algorithm = this.mockery.mock(Algorithm.class);
 
-        MOFitness fitness01 = Fitnesses.create(new MinimisationFitness(6.0), new MinimisationFitness(0.0));
-        final Type type01 = this.mockery.mock(Type.class, "type1_01");
+        MOFitness fitness01 = Fitnesses.create(new MinimisationFitness(3.0), new MinimisationFitness(0.0));
+        final Type type01 = this.mockery.mock(Type.class, "type2_01");
 
-        MOFitness fitness02 = Fitnesses.create(new MinimisationFitness(5.0), new MinimisationFitness(1.0));
-        final Type type02 = this.mockery.mock(Type.class, "type1_02");
-
-        MOFitness fitness03 = Fitnesses.create(new MinimisationFitness(3.0), new MinimisationFitness(3.0));
-        final Type type03 = this.mockery.mock(Type.class, "type1_03");
-
-        MOFitness fitness04 = Fitnesses.create(new MinimisationFitness(0.0), new MinimisationFitness(6.0));
-        final Type type04 = this.mockery.mock(Type.class, "type1_04");
+        MOFitness fitness02 = Fitnesses.create(new MinimisationFitness(2.0), new MinimisationFitness(1.0));
+        final Type type02 = this.mockery.mock(Type.class, "type2_02");
 
         // Cloning returns the mocked objects themselves.
         this.mockery.checking(new Expectations() {
@@ -71,30 +65,22 @@ public class SolutionDistributionTest {
                 will(returnValue(type01));
                 oneOf(type02).getClone();
                 will(returnValue(type02));
-                oneOf(type03).getClone();
-                will(returnValue(type03));
-                oneOf(type04).getClone();
-                will(returnValue(type04));
             }
         });
 
         OptimisationSolution solution01 = new OptimisationSolution(type01, fitness01);
         OptimisationSolution solution02 = new OptimisationSolution(type02, fitness02);
-        OptimisationSolution solution03 = new OptimisationSolution(type03, fitness03);
-        OptimisationSolution solution04 = new OptimisationSolution(type04, fitness04);
 
         Archive archive = Archive.Provider.get();
         archive.add(solution01);
         archive.add(solution02);
-        archive.add(solution03);
-        archive.add(solution04);
 
         Real distribution = new SolutionDistribution().getValue(algorithm);
-        Assert.assertEquals(0.293150984989, distribution.doubleValue(), 0.0000000000001);
+        Assert.assertEquals(0.0, distribution.doubleValue(), 0.0000000000001);
     }
 
     @Test
-    public void resultsPerfectDistribution() {
+    public void testPerfectDistribution() {
         Algorithm algorithm = this.mockery.mock(Algorithm.class);
 
         MOFitness fitness01 = Fitnesses.create(new MinimisationFitness(3.0), new MinimisationFitness(0.0));
@@ -137,6 +123,52 @@ public class SolutionDistributionTest {
 
         Real distribution = new SolutionDistribution().getValue(algorithm);
         Assert.assertEquals(0.0, distribution.doubleValue(), 0.0000000000001);
+    }
+
+    @Test
+    public void testRandomDistribution() {
+        Algorithm algorithm = this.mockery.mock(Algorithm.class);
+
+        MOFitness fitness01 = Fitnesses.create(new MinimisationFitness(6.0), new MinimisationFitness(0.0));
+        final Type type01 = this.mockery.mock(Type.class, "type1_01");
+
+        MOFitness fitness02 = Fitnesses.create(new MinimisationFitness(5.0), new MinimisationFitness(1.0));
+        final Type type02 = this.mockery.mock(Type.class, "type1_02");
+
+        MOFitness fitness03 = Fitnesses.create(new MinimisationFitness(3.0), new MinimisationFitness(3.0));
+        final Type type03 = this.mockery.mock(Type.class, "type1_03");
+
+        MOFitness fitness04 = Fitnesses.create(new MinimisationFitness(0.0), new MinimisationFitness(6.0));
+        final Type type04 = this.mockery.mock(Type.class, "type1_04");
+
+        // Cloning returns the mocked objects themselves.
+        this.mockery.checking(new Expectations() {
+
+            {
+                oneOf(type01).getClone();
+                will(returnValue(type01));
+                oneOf(type02).getClone();
+                will(returnValue(type02));
+                oneOf(type03).getClone();
+                will(returnValue(type03));
+                oneOf(type04).getClone();
+                will(returnValue(type04));
+            }
+        });
+
+        OptimisationSolution solution01 = new OptimisationSolution(type01, fitness01);
+        OptimisationSolution solution02 = new OptimisationSolution(type02, fitness02);
+        OptimisationSolution solution03 = new OptimisationSolution(type03, fitness03);
+        OptimisationSolution solution04 = new OptimisationSolution(type04, fitness04);
+
+        Archive archive = Archive.Provider.get();
+        archive.add(solution01);
+        archive.add(solution02);
+        archive.add(solution03);
+        archive.add(solution04);
+
+        Real distribution = new SolutionDistribution().getValue(algorithm);
+        Assert.assertEquals(0.293150984989, distribution.doubleValue(), 0.0000000000001);
     }
 
     @After
