24b33245be8b05091158982a8c5eaa84ec08dc33
Andries Engelbrecht <engel@up.ac.za>
2012-02-15 11:51:41 +0200

    Unconstrained Function Updates Unit tests added to unconstrained functions, and more comments added
    Signed-off-by: Andries Engelbrecht <engel@up.ac.za>

--- src/test/java/net/sourceforge/cilib/functions/continuous/unconstrained/ShubertTest.java
+++ src/test/java/net/sourceforge/cilib/functions/continuous/unconstrained/ShubertTest.java
@@ -19,44 +19,41 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this library; if not, see <http://www.gnu.org/licenses/>.
  */
-
 package net.sourceforge.cilib.functions.continuous.unconstrained;
 
+import static org.junit.Assert.assertEquals;
 import net.sourceforge.cilib.functions.ContinuousFunction;
 import net.sourceforge.cilib.type.types.container.Vector;
 
+import org.junit.Before;
+import org.junit.Test;
+
 /**
- * <p><b>The Five Uneven Peak Trap Function.</b></p>
  *
- * <p>Global Minimum:
- * <ul>
- * <li>f(x,y) = 0</li>
- * <li>Many global minima where ever x = 0</li>
- * </ul>
- * </p>
- * 
- * <p>Characteristics:
- * <ul>
- * <li>Two-dimensional only</li>
- * <li>Multimodal</li>
- * <li>Non-Separable</li>
- * <li>One global optimum, and nine local optima</li>
- * </ul>
- * </p>
- *
- * R(-0.9,1.2), R(-1.2,1.2)
- *
- * @author  engel
+ * @author engel
  */
-public class Waves implements ContinuousFunction {
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public Double apply(Vector input) {
-        double x = input.doubleValueOf(0);
-        double y = input.doubleValueOf(1);
-        
-        return -(Math.pow(0.3*x, 3)-(y*y-4.5*y*y)*x*y-4.7*Math.cos(3*x-y*y*(2+x))*Math.sin(2.5*Math.PI*x));
+public class ShubertTest {
+ 
+    private ContinuousFunction function;
+
+    @Before
+    public void instantiate() {
+        this.function = new Shubert();
     }
+
+    /** Test for Shubert Function*/
+    @Test
+    public void testEvaluate() {
+        Vector x = Vector.of(0,0);
+        
+        assertEquals(19.875836249, function.apply(x), 0.000000009);
+
+        x.setReal(0, -8.0);
+        x.setReal(1, -8.0);
+        assertEquals(7.507985827, function.apply(x), 0.000000009);
+        
+        x.setReal(0, -7.2);
+        x.setReal(1, -7.7);
+        assertEquals(-157.071676802, function.apply(x), 0.000000009);
+    }   
 }
