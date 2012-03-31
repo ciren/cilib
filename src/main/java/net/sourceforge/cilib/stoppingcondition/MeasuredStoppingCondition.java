593aa3443e434b4c6956deafb7bb6b8cd21692e4
Filipe Nepomuceno <filinep@gmail.com>
2012-03-31 20:28:06 +0200

    Added measured stopping condition and remove Maximum* stopping conditions.

---
+++ src/main/java/net/sourceforge/cilib/stoppingcondition/MeasuredStoppingCondition.java
@@ -0,0 +1,78 @@
+/**
+ * Computational Intelligence Library (CIlib)
+ * Copyright (C) 2003 - 2010
+ * Computational Intelligence Research Group (CIRG@UP)
+ * Department of Computer Science
+ * University of Pretoria
+ * South Africa
+ *
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
+package net.sourceforge.cilib.stoppingcondition;
+
+import net.sourceforge.cilib.algorithm.Algorithm;
+import net.sourceforge.cilib.measurement.Measurement;
+import net.sourceforge.cilib.measurement.generic.Iterations;
+import net.sourceforge.cilib.type.types.Numeric;
+
+public class MeasuredStoppingCondition implements StoppingCondition<Algorithm> {
+    
+    private StoppingPredicate predicate;
+    private double target;
+    private Measurement<? extends Numeric> measurement;
+    
+    public MeasuredStoppingCondition() {
+        this(new Iterations(), new Maximum(), 1000);
+    }
+    
+    public MeasuredStoppingCondition(Measurement measurement, StoppingPredicate predicate, double target) {
+        this.measurement = measurement;
+        this.predicate = predicate;
+        this.target = target;
+    }
+
+    @Override
+    public double getPercentageCompleted(Algorithm algorithm) {
+        return predicate.getPercentage(measurement.getValue(algorithm).doubleValue(), target);
+    }
+
+    @Override
+    public boolean apply(Algorithm algorithm) {
+        return predicate.apply(measurement.getValue(algorithm).doubleValue(), target);
+    }
+
+    public double getTarget() {
+        return target;
+    }
+
+    public StoppingPredicate getPredicate() {
+        return predicate;
+    }
+
+    public Measurement<? extends Numeric> getMeasurement() {
+        return measurement;
+    }
+
+    public void setTarget(double target) {
+        this.target = target;
+    }
+
+    public void setPredicate(StoppingPredicate predicate) {
+        this.predicate = predicate;
+    }
+
+    public void setMeasurement(Measurement<? extends Numeric> measurement) {
+        this.measurement = measurement;
+    }
+}
---
+++ src/main/java/net/sourceforge/cilib/stoppingcondition/Minimum.java
@@ -0,0 +1,36 @@
+/**
+ * Computational Intelligence Library (CIlib)
+ * Copyright (C) 2003 - 2010
+ * Computational Intelligence Research Group (CIRG@UP)
+ * Department of Computer Science
+ * University of Pretoria
+ * South Africa
+ *
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
+package net.sourceforge.cilib.stoppingcondition;
+
+public class Minimum implements StoppingPredicate {
+
+    @Override
+    public double getPercentage(double actualValue, double targetValue) {
+        return 1.0 - actualValue / targetValue;
+    }
+
+    @Override
+    public boolean apply(double actualValue, double targetValue) {
+        return actualValue <= targetValue;
+    }
+
+}
