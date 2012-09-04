6fad17f3ebc2e12b7b6f6c540a960d0798e62fef
Gary Pamparà <gpampara@gmail.com>
2012-09-04 15:21:40 +0200

    Remove redundant identifier for projects in build.
    Signed-off-by: Gary Pamparà <gpampara@gmail.com>

--- project/build.scala
+++ project/build.scala
@@ -3,8 +3,8 @@ import sbt._
 object CIlibBuild extends Build {
   lazy val root = Project("cilib", file(".")) aggregate(library, simulator)
 
-  lazy val library = Project("cilib-library", file("library"))
+  lazy val library = Project("library", file("library"))
 
-  lazy val simulator = Project(id = "cilib-simulator",
+  lazy val simulator = Project(id = "simulator",
                                base = file("simulator")) dependsOn(library)
 }
