cb7910235ee476ca40da0733fd8a4d2b63115013
Paulo Roger <prgc@homer.(none)>
2012-10-03 15:08:57 -0300

    New benchmark function (Chichinadze)
    Created a new benchmark function called Chichinadze. Also a unit test was created.
    Signed-off-by: Paulo Roger <prgc@homer.(none)>

---
+++ library/src/main/java/net/sourceforge/cilib/functions/continuous/unconstrained/Chichinadze.java
@@ -0,0 +1,20 @@
+package net.sourceforge.cilib.functions.continuous.unconstrained;
+
+import net.sourceforge.cilib.functions.ContinuousFunction;
+import net.sourceforge.cilib.type.types.container.Vector;
+
+public class Chichinadze implements ContinuousFunction {
+
+	@Override
+	public Double apply(Vector input) {
+		double x1 = input.get(0).doubleValue();
+		double x2 = input.get(1).doubleValue();
+		
+		double cos = Math.cos(Math.PI * x1 / 2);
+		double sin = Math.sin(5*Math.PI*x1);
+		double ePow = Math.pow(Math.E, -0.5 * ( (x2 - 0.5) * (x2 - 0.5))); 
+		
+		return (x1*x1) - 12 * (x1) + 11 + 10*cos + 8 * sin - Math.pow( (1/5.0), 0.5)* ePow;
+	}
+
+}
---
+++ library/src/test/java/net/sourceforge/cilib/functions/continuous/unconstrained/ChichinadzeTest.java
@@ -0,0 +1,25 @@
+package net.sourceforge.cilib.functions.continuous.unconstrained;
+
+import static org.junit.Assert.assertEquals;
+import net.sourceforge.cilib.functions.ContinuousFunction;
+import net.sourceforge.cilib.type.types.container.Vector;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ChichinadzeTest {
+
+    private ContinuousFunction function;
+
+    @Before
+    public void instantiate() {
+        this.function = new Chichinadze();
+    }
+
+    /** Test of evaluate method, of class Chichinadze. */
+    @Test
+    public void testEvaluate() {
+        Vector x = Vector.of(5.90133, 0.5);
+        assertEquals(-43.31586206998933, function.apply(x), 0);
+    }
+}
