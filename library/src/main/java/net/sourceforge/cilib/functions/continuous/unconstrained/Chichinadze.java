67d89c28eab23aabe6e20ec8c3219a13fee0ae3f
Paulo Roger <prgc@homer.(none)>
2012-10-04 17:37:58 -0300

    Added suggestions for new benchmark functions
    Authors tags were removed and references were added.
    
    Signed-off-by: Paulo Roger <prgc@homer.(none)>

--- library/src/main/java/net/sourceforge/cilib/functions/continuous/unconstrained/Chichinadze.java
+++ library/src/main/java/net/sourceforge/cilib/functions/continuous/unconstrained/Chichinadze.java
@@ -1,8 +1,52 @@
+/**
+ * Computational Intelligence Library (CIlib)
+ * Copyright (C) 2003 - 2010
+ * Computational Intelligence Research Group (CIRG@UP)
+ * Department of Computer Science
+ * University of Pretoria
+ * South Africa
+ *
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
 package net.sourceforge.cilib.functions.continuous.unconstrained;
 
 import net.sourceforge.cilib.functions.ContinuousFunction;
 import net.sourceforge.cilib.type.types.container.Vector;
 
+/**
+ * <p><b>Chichinadze.</b></p>
+ *
+ * <p><b>Reference:</b> S.K. Mishra, <iSome New Test Functions for Global Optimization and Performance of Repulsive Particle
+ * Swarm Methods</i>, Technical report, North-Eastern Hill University, India, 2006</p>
+ *
+ * <p>Minimum:
+ * <ul>
+ * <li> &fnof;(<b>x</b>*) = -43.31586206998933</li>
+ * <li> <b>x</b>* = (5.90133, 0.5)</li>
+ * <li> for x<sub>i</sub> in [-30,30]</li>
+ * </ul>
+ * </p>
+ *
+ * <p>Characteristics:
+ * <ul>
+ * <li>Multimodal</li>
+ * <li>Seperable</li>
+ * <li>Regular</li>
+ * </ul>
+ * </p>
+ * R(-30, 30)^2
+ */
 public class Chichinadze implements ContinuousFunction {
 
 	@Override
--- library/src/main/java/net/sourceforge/cilib/functions/continuous/unconstrained/Zettle.java
+++ library/src/main/java/net/sourceforge/cilib/functions/continuous/unconstrained/Zettle.java
@@ -1,15 +1,50 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Computational Intelligence Library (CIlib)
+ * Copyright (C) 2003 - 2010
+ * Computational Intelligence Research Group (CIRG@UP)
+ * Department of Computer Science
+ * University of Pretoria
+ * South Africa
+ *
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, see <http://www.gnu.org/licenses/>.
  */
 package net.sourceforge.cilib.functions.continuous.unconstrained;
 
 import net.sourceforge.cilib.functions.ContinuousFunction;
 import net.sourceforge.cilib.type.types.container.Vector;
-
 /**
+ * <p><b>Zettle.</b></p>
  *
- * @author Diego Siqueira
+ * <p><b>Reference:</b> S.K. Mishra, <iSome New Test Functions for Global Optimization and Performance of Repulsive Particle
+ * Swarm Methods</i>, Technical report, North-Eastern Hill University, India, 2006</p>
+ *
+ * <p>Minimum:
+ * <ul>
+ * <li> &fnof;(<b>x</b>*) = -0.003791</li>
+ * <li> <b>x</b>* = (-0.0299, 0)</li>
+ * <li> for x<sub>i</sub> in [-5,5]</li>
+ * </ul>
+ * </p>
+ *
+ * <p>Characteristics:
+ * <ul>
+ * <li>Unimodal</li>
+ * <li>Non-Seperable</li>
+ * <li>Regular</li>
+ * </ul>
+ * </p>
+ * R(-5, 5)^2
  */
 public class Zettle implements ContinuousFunction {
     
@@ -17,7 +52,6 @@ public class Zettle implements ContinuousFunction {
 	public Double apply(Vector input) {
 		double x1 = input.get(0).doubleValue();
         double x2 = input.get(1).doubleValue();
-     
         return (Math.pow(((x1 * x1) + (x2 * x2) - 2*x1), 2) + 0.25*x1);   
 	}
 }
