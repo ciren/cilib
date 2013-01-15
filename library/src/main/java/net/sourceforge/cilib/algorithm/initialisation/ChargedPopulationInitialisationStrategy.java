140a264cdb3e157508392bf8bafcf0daa28284e7
Filipe Nepomuceno <filinep@gmail.com>
2013-01-15 17:06:40 +0200

    Fixed a header issue.

--- library/src/main/java/net/sourceforge/cilib/algorithm/initialisation/ChargedPopulationInitialisationStrategy.java
+++ library/src/main/java/net/sourceforge/cilib/algorithm/initialisation/ChargedPopulationInitialisationStrategy.java
@@ -1,11 +1,8 @@
-/**
- * __ __
- * _____ _/ /_/ /_ Computational Intelligence Library (CIlib)
- *   / ___/ / / / __ \ (c) CIRG
- * <p/>
- * @ UP
- *  / /__/ / / / /_/ / http://cilib.net
- * \___/_/_/_/_.___/
+/**           __  __
+ *    _____ _/ /_/ /_    Computational Intelligence Library (CIlib)
+ *   / ___/ / / / __ \   (c) CIRG @ UP
+ *  / /__/ / / / /_/ /   http://cilib.net
+ *  \___/_/_/_/_.___/
  */
 package net.sourceforge.cilib.algorithm.initialisation;
 
