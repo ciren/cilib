13b7b424621b9299efc21bb1d48ac7da3b045e23
Gary Pamparà <gpampara@gmail.com>
2014-06-18 14:37:18 +0200

    Update naming of Position cases: Solution and Point
    Signed-off-by: Gary Pamparà <gpampara@gmail.com>

--- core/src/main/scala/cilib/Entity.scala
+++ core/src/main/scala/cilib/Entity.scala
@@ -10,35 +10,35 @@ sealed abstract class Position[F[_], A] {
 
   def pos =
     this match {
-      case Solution(x)  => x
-      case Entity(x, _) => x
+      case Point(x)  => x
+      case Solution(x, _) => x
     }
 
   def fit =
     this match {
-      case Solution(_)  => None
-      case Entity(_, f) => Some(f)
+      case Point(_)  => None
+      case Solution(_, f) => Some(f)
     }
 
   def map[B](f: A => B)(implicit F: Monad[F]): Position[F, B] =
-    Solution(pos map f)
+    Point(pos map f)
 
   def flatMap[B](f: A => Position[F, B])(implicit F: Monad[F]): Position[F, B] =
-    Solution(F.bind(pos)(f(_).pos))
+    Point(F.bind(pos)(f(_).pos))
 
   def zip[B](other: Position[F, B])(implicit M: Monad[F]): Position[F, (A, B)] =
-    Solution(for {
+    Point(for {
       a <- pos
       b <- other.pos
     } yield (a, b))
 
   def traverse[G[_]: Applicative, B](f: A => G[B])(implicit F: Traverse[F]): G[Position[F, B]] =
-    F.traverse(pos)(f).map(Solution(_))
+    F.traverse(pos)(f).map(Point(_))
 
   def eval(problem: F[A] => Fit) =
     this match {
-      case Solution(x) => Entity(x, problem(x))
-      case Entity(_ ,_) => this
+      case Point(x) => Solution(x, problem(x))
+      case Solution(_ ,_) => this
     }
 }
 
@@ -48,21 +48,21 @@ object Position {
   import spire.implicits._
 
   implicit def positionMonad[F[_]: Monad] = new Monad[({type λ[α] = Position[F,α]})#λ] {
-    def point[A](a: => A): cilib.Position[F,A] = Solution(Applicative[F].point(a))
+    def point[A](a: => A): cilib.Position[F,A] = Point(Applicative[F].point(a))
     def bind[A, B](fa: cilib.Position[F,A])(f: A => cilib.Position[F,B]): cilib.Position[F,B] =
       fa flatMap f
   }
 
-  private final case class Solution[F[_], A](x: F[A]) extends Position[F, A]
-  private final case class Entity[F[_], A](x: F[A], f: Fit) extends Position[F, A]
+  private final case class Point[F[_], A](x: F[A]) extends Position[F, A]
+  private final case class Solution[F[_], A](x: F[A], f: Fit) extends Position[F, A]
 
   implicit class ToPositionVectorOps[F[_], A: Numeric](x: Position[F, A]) {
-    def + (other: Position[F, A])(implicit Z: Zip[F], F: Functor[F]): Position[F, A] = Solution {
+    def + (other: Position[F, A])(implicit Z: Zip[F], F: Functor[F]): Position[F, A] = Point {
       Z.zipWith(x.pos, other.pos)(_ + _)
     }
-    def - (other: Position[F, A])(implicit Z: Zip[F], F: Functor[F]): Position[F, A] = Solution(Z.zipWith(x.pos, other.pos)(_ - _))
+    def - (other: Position[F, A])(implicit Z: Zip[F], F: Functor[F]): Position[F, A] = Point(Z.zipWith(x.pos, other.pos)(_ - _))
     /*def * (other: Position[F, A])(implicit F: Zip[F]) = Solution(x.pos.zipWith(other.pos)((a, ob) => ob.map(_ * a).getOrElse(a))._2) */
-    def *:(scalar: A)(implicit F: Functor[F]): Position[F, A] = Solution(x.pos.map(_ * scalar))
+    def *:(scalar: A)(implicit F: Functor[F]): Position[F, A] = Point(x.pos.map(_ * scalar))
   }
 
   implicit def positionFitness[F[_], A] = new Fitness[Position[F, A]] {
@@ -72,7 +72,7 @@ object Position {
 
   // Smart constructor
   def apply[A](xs: List[A]): Position[List, A] =
-    Solution(xs)
+    Point(xs)
 
 }
 
