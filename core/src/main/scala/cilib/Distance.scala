47013776610ccec6e03faf44d602d83f28d5e181
Robert Garden <robgarden@gmail.com>
2015-11-03 21:31:06 +0100

    Hamming distance

--- core/src/main/scala/cilib/Distance.scala
+++ core/src/main/scala/cilib/Distance.scala
@@ -9,19 +9,20 @@ import spire.algebra.{Field,NRoot,Order,Ring,Signed}
 
 object Distance {
 
-  type Distance[F[_],A] = (F[A], F[A]) => A
-
-  def chebyshev[F[_]: Foldable, A: Order : Signed](implicit ev: Ring[A]): Distance[F,A] =
+  def chebyshev[F[_]: Foldable, A: Order : Signed](implicit ev: Ring[A]) =
     (a: F[A], b: F[A]) => (a.toList zip b.toList)
       .map { case (ai, bi) => abs(ai - bi) }
       .foldLeft(ev.zero) { (ai, bi) => max(ai, bi) }
 
-  def minkowski[F[_]: Foldable, A: NRoot : Signed](alpha: Int)(implicit ev: Field[A]): Distance[F,A] =
+  def minkowski[F[_]: Foldable, A: NRoot : Signed](alpha: Int)(implicit ev: Field[A]) =
     (a: F[A], b: F[A]) => (a.toList zip b.toList)
       .map { case (ai, bi) => abs(ai - bi) ** alpha }
       .foldLeft(ev.zero) { (ai, bi) => (ai + bi) ** (1.0 / alpha) }
 
-  def manhattan[F[_]: Foldable, A: Field : NRoot : Signed]: Distance[F,A] = minkowski[F,A](1)
-  def euclidean[F[_]: Foldable, A: Field : NRoot : Signed]: Distance[F,A] = minkowski[F,A](2)
+  def manhattan[F[_]: Foldable, A: Field : NRoot : Signed] = minkowski[F,A](1)
+  def euclidean[F[_]: Foldable, A: Field : NRoot : Signed] = minkowski[F,A](2)
+
+  def hamming[F[_]: Foldable, A] =
+    (a: F[A], b: F[A]) => (a.toList zip b.toList).filter { case (ai, bi) => ai != bi }.size
 
 }
