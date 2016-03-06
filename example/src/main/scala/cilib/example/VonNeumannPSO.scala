208e24d151f40dbe4ce1b988cdaac1188d627e7c
Gary Pamparà <gpampara@gmail.com>
2016-03-06 13:27:26 +0200

    PSO using Von Neumann topology function
    Signed-off-by: Gary Pamparà <gpampara@gmail.com>

---
+++ example/src/main/scala/cilib/example/VonNeumannPSO.scala
@@ -0,0 +1,43 @@
+package cilib
+package example
+
+import cilib.pso._
+import cilib.pso.Defaults._
+
+import scalaz.NonEmptyList
+import scalaz.effect._
+import scalaz.effect.IO.putStrLn
+import scalaz.std.list._
+import spire.implicits._
+import spire.math.Interval
+
+import cilib.syntax.algorithm._
+
+import scalaz._
+import Scalaz._
+
+object VonNeumannPSO extends SafeApp {
+
+  // Create a problem by specifiying the function and it's constrainment
+  val sum = Problems.spherical[Double]
+
+  // Define a normal GBest PSO and run it for a single iteration
+  val cognitive = Guide.pbest[Mem[Double],Double]
+  val social = Guide.vonNeumann[Mem[Double]]
+  val gbestPSO = gbest(0.729844, 1.496180, 1.496180, cognitive, social)
+
+  // RVar
+  val swarm = Position.createCollection(PSO.createParticle(x => Entity(Mem(x, x.zeroed), x)))(Interval(-5.12,5.12)^30, 20)
+  val iter = Iteration.sync(gbestPSO)
+
+  val opt = Comparison.dominance(Min)
+
+  // Our IO[Unit] that runs the algorithm, at the end of the world
+  override val runc: IO[Unit] = {
+    val result = Runner.repeat(1000, iter, swarm).run(opt)(sum).run(RNG.fromTime)
+    val positions = result._2.map(x => Lenses._position.get(x))
+
+    putStrLn(result.toString)
+  }
+
+}
