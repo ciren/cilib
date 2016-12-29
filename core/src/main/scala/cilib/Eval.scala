4ac9a87e58e6255a596dc9358997495ccb61c814
Gary Pamparà <gpampara@gmail.com>
2016-12-29 08:00:36 +0200

    Allow Eval instances to have a textual name
    Using the functions to create Eval instances creates some
    anonymous classes that allow for and unallow constrainment.
    These instances are simply memory addresses, when `toString`
    is called on the references. For usage in the REPL, and for
    general tracing purposes, a name for the instance would be
    useful, with a reasonable default name also defined.
    
    Signed-off-by: Gary Pamparà <gpampara@gmail.com>

--- core/src/main/scala/cilib/Eval.scala
+++ core/src/main/scala/cilib/Eval.scala
@@ -16,34 +16,47 @@ sealed abstract class Eval[A] { // This represents the function (NonEmpty)List[A
 }
 
 object Eval {
-  def unconstrained[F[_],A](f: F[A] => Double)(implicit F: Input[F]): Eval[A] = new Eval[A] {
-    def eval(a: NonEmptyList[A]): RVar[Objective[A]] = RVar.point {
-      Single(Feasible(f(F.toInput(a))), List.empty)
-    }
 
-    def constrainBy(cs: List[Constraint[A,Double]]): Eval[A] =
-      constrained(f, cs)
+  def unconstrained[F[_]:Input, A](f: F[A] => Double) =
+    unconstrainedNamed(f, "unnamed")
 
-    def unconstrain = this
-  }
-
-  def constrained[F[_],A](f: F[A] => Double, cs: List[Constraint[A,Double]])(implicit F: Input[F]): Eval[A] = new Eval[A] {
-    import spire.algebra.Eq
-    import spire.implicits._
-
-    def eval(a: NonEmptyList[A]): RVar[Objective[A]] = RVar.point {
-      cs.filter(c => !Constraint.satisfies(c, a)) match {
-        case Nil => Single(Feasible(f(F.toInput(a))), List.empty)
-        case xs  => Single(Infeasible(f(F.toInput(a)), xs.length), xs)
+  def unconstrainedNamed[F[_],A](f: F[A] => Double, name: String)(implicit F: Input[F]): Eval[A] =
+    new Eval[A] {
+      def eval(a: NonEmptyList[A]): RVar[Objective[A]] = RVar.point {
+        Single(Feasible(f(F.toInput(a))), List.empty)
       }
+
+      def constrainBy(cs: List[Constraint[A,Double]]): Eval[A] =
+        constrainedNamed(f, cs, name)
+
+      def unconstrain = this
+
+      override def toString = "Unconstrained(" + name + ")"
     }
 
-    def constrainBy(css: List[Constraint[A,Double]]): Eval[A] =
-      constrained(f, css)
+  def constrained[F[_]:Input, A](f: F[A] => Double, cs: List[Constraint[A,Double]]): Eval[A] =
+    constrainedNamed(f, cs, "unnamed")
 
-    def unconstrain =
-      unconstrained(f)
-  }
+  def constrainedNamed[F[_],A](f: F[A] => Double, cs: List[Constraint[A,Double]], name: String)(implicit F: Input[F]): Eval[A] =
+    new Eval[A] {
+      import spire.algebra.Eq
+      import spire.implicits._
+
+      def eval(a: NonEmptyList[A]): RVar[Objective[A]] = RVar.point {
+        cs.filter(c => !Constraint.satisfies(c, a)) match {
+          case Nil => Single(Feasible(f(F.toInput(a))), List.empty)
+          case xs  => Single(Infeasible(f(F.toInput(a)), xs.length), xs)
+        }
+      }
+
+      def constrainBy(css: List[Constraint[A,Double]]): Eval[A] =
+        constrainedNamed(f, css, name)
+
+      def unconstrain =
+        unconstrainedNamed(f, name)
+
+      override def toString = "Constrained(" + name + ")"
+    }
 
   implicit val nelInput = new Input[NonEmptyList] {
     def toInput[A](a: NonEmptyList[A]): NonEmptyList[A] = a
