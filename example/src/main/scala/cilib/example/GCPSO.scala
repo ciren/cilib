90a25c60b61a6c3fcdedfef13f1065fbdfafb507
Gary Pamparà <gpampara@gmail.com>
2017-11-24 17:00:53 +0200

    Add example of GCPSO
    There was no exmaple of using an algorithm that uses a `StepS`
    structure internally. The GCPSO is, however, such an algorithm
    and the added example shows the usage to be as simple as other
    algorithms. The only additional concern is the set of algorithm
    parameters that the algorithm itself needs to execute.
    
    These additional parameters are passed in first, allowing the
    computation to yield a `Step` which then runs the same as in other
    examples.
    
    Signed-off-by: Gary Pamparà <gpampara@gmail.com>

---
+++ example/src/main/scala/cilib/example/GCPSO.scala
@@ -0,0 +1,46 @@
+package cilib
+package example
+
+import cilib.pso._
+import cilib.pso.Defaults._
+import Eval._
+
+import scalaz.NonEmptyList
+import scalaz.effect._
+import scalaz.effect.IO.putStrLn
+import spire.implicits._
+import spire.math.Interval
+
+import scalaz._
+
+object GCPSO extends SafeApp {
+
+  // Create a problem by specifiying the function and it's constrainment
+  val sum = Eval.unconstrained(cilib.benchmarks.Benchmarks.spherical[NonEmptyList, Double]).eval
+
+  // Define a normal GBest PSO and run it for a single iteration
+  val cognitive = Guide.pbest[Mem[Double],Double]
+  val social = Guide.gbest[Mem[Double]]
+  val gbestPSO: List[Particle[Mem[Double],Double]] => Particle[Mem[Double],Double] => StepS[Double, PSO.GCParams, Particle[Mem[Double],Double]] =
+    gcpso(0.729844, 1.496180, 1.496180, cognitive)
+
+  val iter:  Kleisli[StepS[Double, PSO.GCParams, ?], List[Particle[Mem[Double],Double]], List[Particle[Mem[Double],Double]]] =
+    Iteration.syncS(gbestPSO)
+
+  val swarm = Position.createCollection(PSO.createParticle(x => Entity(Mem(x, x.zeroed), x)))(Interval(-5.12,5.12)^30, 20)
+  val opt = Comparison.dominance(Min)
+
+  // Our IO[Unit] that runs the algorithm, at the end of the world
+  override val runc: IO[Unit] = {
+    val algParams = PSO.defaultGCParams
+
+    val result =
+      Runner.repeatS(1000, iter, swarm)
+        .run(algParams)
+        .run(opt)(sum)
+        .run(RNG.fromTime)
+
+    putStrLn(result.toString)
+  }
+
+}
--- exec/src/main/scala/cilib/Runner.scala
+++ exec/src/main/scala/cilib/Runner.scala
@@ -6,7 +6,12 @@ import Scalaz._
 object Runner {
 
   def repeat[A,B](n: Int, alg: Kleisli[Step[A,?],List[B],List[B]], collection: RVar[List[B]]): Step[A,List[B]] =
-    Step.pointR(collection).flatMap(coll => (1 to n).toStream.foldLeftM[Step[A,?],List[B]](coll) { (a, c) =>
+    Step.pointR(collection).flatMap(coll => (1 to n).toStream.foldLeftM[Step[A,?],List[B]](coll) { (a, _) =>
+      alg.run(a).map(_.toList)
+    })
+
+  def repeatS[A,S,B](n: Int, alg: Kleisli[StepS[A,S,?],List[B],List[B]], collection: RVar[List[B]]): StepS[A,S,List[B]] =
+    StepS.pointR(collection).flatMap(coll => (1 to n).toStream.foldLeftM[StepS[A,S,?],List[B]](coll) { (a, _) =>
       alg.run(a).map(_.toList)
     })
 }
