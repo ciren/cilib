d8971127016014e5f2a39ca454f1f249a2c4300f
Gary Pamparà <gpampara@gmail.com>
2017-12-18 09:03:49 +0200

    Capture Input instance on Eval creation
    The implicit parameter group can cause erroneous usage. It is better to
    capture the Input instance at creation instead of deferring to the
    call site so that this implicit parameter can be removed.
    
    Signed-off-by: Gary Pamparà <gpampara@gmail.com>

--- core/src/main/scala/cilib/Eval.scala
+++ core/src/main/scala/cilib/Eval.scala
@@ -12,13 +12,15 @@ sealed abstract class Eval[F[_],A] {
 
   import Eval._
 
+  val F: Input[F]
+
   def run: F[A] => Double
 
-  def eval(implicit F: Input[F]): RVar[NonEmptyList[A] => Objective[A]] =
+  def eval: RVar[NonEmptyList[A] => Objective[A]] =
     RVar.point { (fa: NonEmptyList[A]) =>
       this match {
-        case Unconstrained(f)   => Single(Feasible(f(F.toInput(fa))), List.empty)
-        case Constrained(f, cs) =>
+        case Unconstrained(f, _)   => Single(Feasible(f(F.toInput(fa))), List.empty)
+        case Constrained(f, cs, _) =>
           cs.filter(c => !Constraint.satisfies(c, fa)) match {
             case Nil => Single(Feasible(f(F.toInput(fa))), List.empty)
             case xs  => Single(Infeasible(f(F.toInput(fa)), xs.length), xs)
@@ -27,74 +29,21 @@ sealed abstract class Eval[F[_],A] {
     }
 
   def constrain(cs: List[Constraint[A]]): Eval[F,A] =
-    Constrained(run, cs)
+    Constrained(run, cs, F)
 
   def unconstrain: Eval[F,A] =
-    Unconstrained(run)
+    Unconstrained(run, F)
 }
 
 object Eval {
-  private final case class Unconstrained[F[_],A](run: F[A] => Double) extends Eval[F,A]
-  private final case class Constrained[F[_],A](run: F[A] => Double, cs: List[Constraint[A]]) extends Eval[F,A]
+  private final case class Unconstrained[F[_],A](run: F[A] => Double, F: Input[F]) extends Eval[F,A]
+  private final case class Constrained[F[_],A](run: F[A] => Double, cs: List[Constraint[A]], F: Input[F]) extends Eval[F,A]
 
-  def unconstrained[F[_],A](f: F[A] => Double): Eval[F,A] = Unconstrained(f)
-  def constrained[F[_],A](f: F[A] => Double, cs: List[Constraint[A]]): Eval[F,A] = Constrained(f, cs)
+  def unconstrained[F[_]: Input,A](f: F[A] => Double)(implicit F: Input[F]): Eval[F,A] =
+    Unconstrained(f, F)
 
-  // def unconstrained[F[_], A](f: F[A] => Double)(implicit F: Input[F]): RVar[NonEmptyList[A] => Objective[A]] =
-  //   RVar.point {
-  //     (fa: NonEmptyList[A]) => Single(Feasible(f(F.toInput(fa))), List.empty)
-  //   }
-
-  // def constrain[F[_], A](f: F[A] => Double, cs: List[Constraint[A,Double]])(implicit F: Input[F], A: spire.algebra.Eq[Double]): RVar[NonEmptyList[A] => Objective[A]] = // indicate that this is already constrained?
-  //   RVar.point {
-  //     (fa: NonEmptyList[A]) =>
-  //       cs.filter(c => !Constraint.satisfies(c, fa)) match {
-  //         case Nil => Single(Feasible(f(F.toInput(fa))), List.empty)
-  //         case xs  => Single(Infeasible(f(F.toInput(fa)), xs.length), xs)
-  //       }
-  //   }
-
-
-  // def unconstrained[F[_]:Input, A](f: F[A] => Double) =
-  //   unconstrainedNamed(f, "unnamed")
-
-  // def unconstrainedNamed[F[_],A](f: F[A] => RVar[Double], name: String)(implicit F: Input[F]): Eval[A] =
-  //   new Eval[A] {
-  //     def eval(a: NonEmptyList[A]): RVar[Objective[A]] =
-  //       f(F.toInput(a)).map(x => Feasible(x), List.empty)
-  //     // RVar.point {
-  //     //   Single(Feasible(f(F.toInput(a))), List.empty)
-  //     // }
-
-  //     def unconstrain = this
-
-  //     override def toString = "Unconstrained(" + name + ")"
-  //   }
-
-  // def constrained[F[_]:Input, A](f: F[A] => Double, cs: List[Constraint[A,Double]]): Eval[A] =
-  //   constrainedNamed(f, cs, "unnamed")
-
-  // def constrainedNamed[F[_],A](f: F[A] => Double, cs: List[Constraint[A,Double]], name: String)(implicit F: Input[F]): Eval[A] =
-  //   new Eval[A] {
-  //     import spire.algebra.Eq
-  //     import spire.implicits._
-
-  //     def eval(a: NonEmptyList[A]): RVar[Objective[A]] = RVar.point {
-  //       cs.filter(c => !Constraint.satisfies(c, a)) match {
-  //         case Nil => Single(Feasible(f(F.toInput(a))), List.empty)
-  //         case xs  => Single(Infeasible(f(F.toInput(a)), xs.length), xs)
-  //       }
-  //     }
-
-  //     def constrainBy(css: List[Constraint[A,Double]]): Eval[A] =
-  //       constrainedNamed(f, css, name)
-
-  //     def unconstrain =
-  //       unconstrainedNamed(f, name)
-
-  //     override def toString = "Constrained(" + name + ")"
-  //   }
-
+  def constrained[F[_]: Input,A](f: F[A] => Double, cs: List[Constraint[A]])(implicit F: Input[F]): Eval[F,A] =
+    Constrained(f, cs, F)
 }
 
 trait EvalInstances {
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -10,4 +10,4 @@ addSbtPlugin("org.tpolecat" % "tut-plugin" % "0.6.2")
 
 addSbtPlugin("com.dwijnand" % "sbt-travisci" % "1.1.1")
 
-addSbtPlugin("io.get-coursier" % "sbt-coursier" % "1.0.0-RC13")
+addSbtPlugin("io.get-coursier" % "sbt-coursier" % "1.0.0")
