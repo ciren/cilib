d0cc02ff1e325d86a57d102b8136437c016bfde6
Gary Pamparà <gpampara@gmail.com>
2017-12-20 15:36:55 +0200

    Move script into separate file
    Signed-off-by: Gary Pamparà <gpampara@gmail.com>

---
+++ project/deploy-site.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sbt "++${TRAVIS_SCALA_VERSION}" "^^${SBT_VERSION}" publish docs/makeSite
+
+mkdir _site
+cd _site
+git clone git@github.com:cirg-up/cilib.git --branch gh-pages --depth 1 .
+git checkout --orphan newBranch
+cp -R ../docs/target/site .
+git add --all
+git commit -a -m "Travis #$TRAVIS_BUILD_NUMBER update site"
+git branch -D gh-pages
+git branch -m gh-pages
+git push --force origin gh-pages
