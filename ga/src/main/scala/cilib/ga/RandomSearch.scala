45d90dcc72e73fbcec900edda4a7a4d3fbc973b4
Kyle Erwin <kyle.erwin24@gmail.com>
2018-01-04 17:49:13 +0200

    Compatible with the latest cilib

--- ga/src/main/scala/cilib/ga/RandomSearch.scala
+++ ga/src/main/scala/cilib/ga/RandomSearch.scala
@@ -1,15 +1,13 @@
 package cilib
 package ga
 
-import cilib.Lenses._
-
 import scalaz.std.list._
 import scalaz.syntax.traverse._
 
 // Author: Kyle Erwin
 object RandomSearch {
 
-    def ga[S](
+    def ga(
         parentSelection: List[Individual] => RVar[List[Individual]],
         distribution: Double => RVar[Double]): List[Individual] => Individual => Step[Double, List[Individual]] =
         GA.ga(1.0, parentSelection, crossover, mutation(distribution))
@@ -18,7 +16,7 @@ object RandomSearch {
     // The mutation method is applied to all the parents.
     // The parents are mutated by adding a random value gaussian distribution to
     // point in their position.
-    def mutation(distribution: Double => RVar[Double])(parents: List[Individual[S]]): RVar[List[Individual[S]]] = 
+    def mutation(distribution: Double => RVar[Double])(parents: List[Individual]): RVar[List[Individual]] = 
         parents.traverse(parent =>
             Lenses._position.modifyF((p: Position[Double]) => p.traverse(distribution))(parent))
             
