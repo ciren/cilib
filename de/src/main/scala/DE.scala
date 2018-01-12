9d4864ca6f07ecf51805aea7a0762e5c9511c15b
Gary Pamparà <gpampara@gmail.com>
2018-01-12 12:50:33 +0200

    bin and exp pivot points for DE
    Signed-off-by: Gary Pamparà <gpampara@gmail.com>

--- de/src/main/scala/DE.scala
+++ de/src/main/scala/DE.scala
@@ -7,12 +7,10 @@ import spire.implicits._
 
 import scalaz._
 import Scalaz._
-import monocle._
-import Monocle._
 
 object DE {
 
-  def de[S,A:Numeric:Rng](
+  def de[S,A:Numeric](
     p_r: Double,
     p_m: Double,
     targetSelection: NonEmptyList[Individual[S,A]] => RVar[Individual[S,A]]
@@ -22,7 +20,7 @@ object DE {
     collection => x => for {
       evaluated <- Step.eval((a: Position[A]) => a)(x)
       trial <- Step.pointR(basicMutation(Numeric[A].fromDouble(p_m), targetSelection, collection, x))
-      pivots <- Step.pointR(bin(p_r, evaluated))
+      pivots <- Step.pointR(bin(p_r, evaluated.pos))
       offspring = crossover(x, trial, pivots)
       evaluatedOffspring <- Step.eval((a: Position[A]) => a)(offspring)
       fittest <- better(evaluated, evaluatedOffspring)
@@ -58,7 +56,6 @@ object DE {
             RVar.point(List.empty)
         })
 
-
       for {
         t <- target
         p <- pairs
@@ -69,34 +66,43 @@ object DE {
   def crossover[S,A](target: Individual[S,A], trial: Position[A], pivots: NonEmptyList[Boolean]) =
     target.copy(
       pos = {
-        target.pos.zip(trial).zip(Position(pivots, trial.boundary)).map { case ((a,b), p) => // Position apply function :(
-          if (p) b else a
+        target.pos.zip(trial).zip(Position(pivots, trial.boundary)).map {
+          case ((a,b), p) => // Position apply function :(
+            if (p) b else a
         }
       })
 
 
-  // This is not a nice implementation ??? -> it feels far too low level and inelegant
-  def bin[S,A:Rng](
+  def bin[F[_]:Foldable1,A](
     p_r: Double,
-    parent: Individual[S,A]
+    parent: F[A]
   ): RVar[NonEmptyList[Boolean]] =
+    Dist.uniformInt(spire.math.Interval(0, parent.length - 1))
+      .flatMap(j => parent.toNel.zipWithIndex.traverse {
+        case (_, i) =>
+          if (i == j) RVar.point(true)
+          else Dist.stdUniform.map(_ < p_r)
+      })
+
+
+  def exp[F[_]:Foldable1,A](
+    p_r: Double,
+    parent: F[A]
+  ): RVar[NonEmptyList[Boolean]] = {
+    val length = parent.length
     for {
-      j <- Dist.uniformInt(spire.math.Interval(0, parent.pos.pos.size - 1))
-      l <- parent.pos.pos.toList.traverse(_ => Dist.stdUniform.map(_ < p_r))
+      start <- Dist.uniformInt(spire.math.Interval(0, length - 1))
+      circular = Stream.continually((0 to length).toStream).flatten
+      randoms <- Dist.stdUniform.replicateM(length)
     } yield {
-      val t: List[Boolean] = l.applyOptional(index(j)).set(true)
-      NonEmptyList(t.head, t.tail: _*)
+      val paired = circular.drop(start).take(length).toList.zip(randoms)
+      val adjacent = paired.head :: paired.tail.takeWhile(t => t._2 < p_r)
+
+      (0 to length).foldRight(List.empty[Boolean])(
+        (c, a) => adjacent.find(_._1 == c) match {
+          case None => false :: a
+          case Some(_) => true :: a
+        }).toNel.getOrElse(sys.error("Impossible -> there has to be at least 1 element"))
     }
-
-  // def exp[A:Rng](
-  //   p_r: Double,
-  //   parent: Individual[A]
-  // ): RVar[List[Boolean]] =
-  //   for {
-  //     start <- Dist.uniformInt(0, parent.pos.pos.size - 1)
-  //     length = parent.pos.pos.size - 1
-  //     circular = Stream.continually((0 to length).toList).drop(start).take(length)
-  //     list <-
-
-
+  }
 }
