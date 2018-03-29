49cf33ac3a2d11f39a14b2a84cea3323dfc266b8
Kyle Erwin <kyle.erwin24@gmail.com>
2018-03-29 08:04:03 +0200

    Formating

--- pso/src/main/scala/cilib/pso/PSO.scala
+++ pso/src/main/scala/cilib/pso/PSO.scala
@@ -9,6 +9,9 @@ import Scalaz._
 import spire.algebra._
 import spire.implicits._
 
+import shapeless._
+import ops.tuple.FlatMapper
+
 object PSO {
   import Lenses._
 
@@ -54,8 +57,7 @@ object PSO {
     if (b) updatePBest(p) else Step.pure(p)
   }
 
-    import shapeless._
-    import ops.tuple.FlatMapper
+
 
     trait LowPriorityFlatten extends Poly1 {
         implicit def default[T] = at[T](Tuple1(_))
@@ -85,16 +87,16 @@ object PSO {
         Lenses._vector[A].set(c)(p)
     })(x)
 
-def initToGB[S,A:scalaz.Equal](x: Entity[S,A], gBest: Position[A])(implicit N: spire.math.Numeric[A]) = 
+  def initToGB[S,A:scalaz.Equal](x: Entity[S,A], gBest: Position[A])(implicit N: spire.math.Numeric[A]) = 
     Lenses._position.modify((p: Position[A]) => {
-        val c: NonEmptyList[A] = p.pos zip p.boundary zip gBest.pos map {x => flatten(x)} map {
-            case t if t._2.contains(N.toDouble(t._1)) => t._1
-            case t if t._2.doesNotContain(N.toDouble(t._1)) => t._3
-        }
-        Lenses._vector[A].set(c)(p)
+      val c: NonEmptyList[A] = p.pos zip p.boundary zip gBest.pos map {x => flatten(x)} map {
+          case t if t._2.contains(N.toDouble(t._1)) => t._1
+          case t if t._2.doesNotContain(N.toDouble(t._1)) => t._3
+      }
+      Lenses._vector[A].set(c)(p)
     })(x)
 
-def zeroedVelocity[S,A:scalaz.Equal](x: Entity[S,A])(
+  def zeroedVelocity[S,A:scalaz.Equal](x: Entity[S,A])(
     implicit N: spire.math.Numeric[A], V: HasVelocity[S, A]) =
     Entity(V._velocity.modify((p: Position[A]) => {
         val c: NonEmptyList[A] = p.pos zip p.boundary zip V._velocity.get(x.state).pos map {x => flatten(x)} map {
@@ -105,7 +107,7 @@ def zeroedVelocity[S,A:scalaz.Equal](x: Entity[S,A])(
     })(x.state), x.pos)
 
 
-def reverseVelocity[S,A:scalaz.Equal](x: Entity[S,A])(
+  def reverseVelocity[S,A:scalaz.Equal](x: Entity[S,A])(
     implicit N: spire.math.Numeric[A], V: HasVelocity[S, A]) =
     Entity(V._velocity.modify((p: Position[A]) => {
         val c: NonEmptyList[A] = p.pos zip p.boundary zip V._velocity.get(x.state).pos map {x => flatten(x)} map {
