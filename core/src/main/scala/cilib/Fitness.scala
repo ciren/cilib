374d943ca475e8bb556777e05c2b5fd509922244
Gary Pamparà <gpampara@gmail.com>
2018-07-18 11:10:43 +0200

    Simplification of fitness logic
    The logic for "how to do a comparison" should be within the
    `Comparison` values (such as `dominance`), and not in the optimisation
    scheme target (Min or Max)

--- core/src/main/scala/cilib/Fitness.scala
+++ core/src/main/scala/cilib/Fitness.scala
@@ -21,7 +21,7 @@ sealed abstract class Fit {
 }
 
 final case class Feasible(v: Double) extends Fit
-final case class Infeasible(v: Double /*, violations: Int*/ ) extends Fit
+final case class Infeasible(v: Double) extends Fit
 final case class Adjusted private[cilib] (original: Feasible \/ Infeasible, adjust: Double)
     extends Fit
 @annotation.implicitNotFound(
@@ -36,25 +36,55 @@ abstract class Comparison(val opt: Opt) {
 
 object Comparison {
 
-  def compare[F[_], A](x: F[A], y: F[A])(implicit F: Fitness[F, A, A]): Comparison => F[A] =
+  def fitCompare(opt: Opt)(x: Fit, y: Fit, xv: => Int, yv: => Int) =
+    (x, y) match {
+      case (Adjusted(_, a), Adjusted(_, b)) => opt.D.order(a, b)
+      case (Adjusted(_, a), Feasible(b))    => opt.D.order(a, b)
+      case (Adjusted(_, _), Infeasible(_))  => GT
+      case (Feasible(a), Adjusted(_, b))    => opt.D.order(a, b)
+      case (Feasible(a), Feasible(b))       => opt.D.order(a, b)
+      case (Feasible(_), Infeasible(_))     => GT
+      case (Infeasible(_), Adjusted(_, _))  => LT
+      case (Infeasible(_), Feasible(_))     => LT
+      case (Infeasible(a), Infeasible(b)) =>
+        if (xv == yv) opt.D.order(a, b)
+        else opt.I.order(xv, yv)
+    }
+
+  def multiFitCompare(opt: Opt)(xs: List[Fit], ys: List[Fit], xsv: => Int, ysv: => Int) = {
+    val z = xs.zip(ys)
+    val x2 = z.forall {
+      case (a, b) =>
+        val r = fitCompare(opt)(a, b, xsv, ysv)
+        r == GT || r == EQ
+    }
+    val y2 = z.exists {
+      case (a, b) =>
+        fitCompare(opt)(a, b, xsv, ysv) == GT
+    }
+
+    if (x2 && y2) GT else if (x2) EQ else LT
+  }
+
+  def compare[F[_], A, B](x: F[A], y: F[A])(implicit F: Fitness[F, A, B]): Comparison => F[A] =
     _.apply(x, y)
 
-  def dominance(o: Opt) = new Comparison(o) {
-    def apply[F[_], A, B](a: F[A], b: F[A])(implicit F: Fitness[F, A, B]) = {
-      def fromOrdering(f1: Objective[B], f2: Objective[B]): F[A] =
-        o.order(f1, f2) match {
-          case LT => b
-          case GT => a
-          case EQ => a // ??? No difference?
-        }
-
+  def dominance(opt: Opt) = new Comparison(opt) {
+    def apply[F[_], A, B](a: F[A], b: F[A])(implicit F: Fitness[F, A, B]) =
       (F.fitness(a), F.fitness(b)) match {
-        case (Some(f1), Some(f2)) => fromOrdering(f1, f2)
-        case (None, None)         => a
-        case (None, _)            => b
-        case (_, None)            => a
+        case (Some(f1), Some(f2)) =>
+          (f1.fitness, f2.fitness) match {
+            case (-\/(x), -\/(y)) =>
+              if (fitCompare(opt)(x, y, f1.violationCount, f2.violationCount) === GT) a else b
+            case (\/-(x), \/-(y)) =>
+              val r = multiFitCompare(opt)(x, y, f1.violationCount, f2.violationCount)
+              if (r != LT) a else b
+            case _ => a
+          }
+        case (None, None) => a
+        case (None, _)    => b
+        case (_, None)    => a
       }
-    }
   }
 
   // Dominance is the generalised form of normal quality comparisons, taking constraint violations into account
@@ -62,15 +92,9 @@ object Comparison {
     dominance(o)
 
   def fitter[F[_], A, B](x: F[A], y: F[A])(implicit F: Fitness[F, A, B]): Comparison => Boolean =
-    cmp =>
-      (F.fitness(x), F.fitness(y)) match {
-        case (Some(a), Some(b)) => cmp.opt.objectiveOrder[B].order(a, b) === GT
-        case (None, None)       => false
-        case (None, Some(_))    => false
-        case (Some(_), None)    => true
-    }
+    compare(x, y).andThen(_ == x)
 
-  def fittest[F[_], A](a: F[A], b: F[A])(implicit F: Fitness[F, A, A]): Step[A, F[A]] =
+  def fittest[F[_], A, B](a: F[A], b: F[A])(implicit F: Fitness[F, A, B]): Step[A, F[A]] =
     Step.withCompareR(comp =>
       RVar.pure {
         if (fitter(a, b).apply(comp)) a else b
@@ -78,92 +102,15 @@ object Comparison {
 
 }
 
-sealed trait Opt {
-  def objectiveOrder[A]: Order[Objective[A]]
-
-  final def order[A](x: Objective[A], y: Objective[A]): Ordering =
-    objectiveOrder[A].order(x, y)
+sealed abstract class Opt {
+  def D: Order[Double]
+  val I = Order[Int].reverseOrder
 }
 
 final case object Min extends Opt {
-  private val D = Order[Double].reverseOrder
-  private val I = Order[Int].reverseOrder
-
-  private def fitCompare(x: Fit, y: Fit, xv: => Int, yv: => Int) =
-    (x, y) match {
-      case (Adjusted(_, a), Adjusted(_, b)) => D.order(a, b)
-      case (Adjusted(_, a), Feasible(b))    => D.order(a, b)
-      case (Adjusted(_, _), Infeasible(_))  => GT
-      case (Feasible(a), Adjusted(_, b))    => D.order(a, b)
-      case (Feasible(a), Feasible(b))       => D.order(a, b)
-      case (Feasible(_), Infeasible(_))     => GT
-      case (Infeasible(_), Adjusted(_, _))  => LT
-      case (Infeasible(_), Feasible(_))     => LT
-      case (Infeasible(a), Infeasible(b)) =>
-        if (xv == yv) D.order(a, b)
-        else I.order(xv, yv)
-    }
-
-  def objectiveOrder[A] = new Order[Objective[A]] {
-    def order(x: Objective[A], y: Objective[A]) =
-      (x.fitness, y.fitness) match {
-        case (-\/(f1), -\/(f2)) => fitCompare(f1, f2, x.violationCount, y.violationCount)
-        case (\/-(xs), \/-(ys)) =>
-          val z = xs.zip(ys)
-          val x2 = z.forall {
-            case (a, b) =>
-              val r = fitCompare(a, b, x.violationCount, y.violationCount)
-              r == LT || r == EQ
-          }
-          val y2 = z.exists {
-            case (a, b) => fitCompare(a, b, x.violationCount, y.violationCount) == LT
-          }
-
-          if (x2 && y2) LT else if (x2) EQ else GT
-
-        case _ => EQ
-      }
-  }
+  val D = Order[Double].reverseOrder
 }
 
 final case object Max extends Opt {
-  private val D = Order[Double]
-  private val I = Order[Int]
-
-  protected def fitCompare(x: Fit, y: Fit, xv: => Int, yv: => Int) =
-    (x, y) match {
-      case (Adjusted(_, a), Adjusted(_, b)) => D.order(a, b)
-      case (Adjusted(_, a), Feasible(b))    => D.order(a, b)
-      case (Adjusted(_, _), Infeasible(_))  => GT
-      case (Feasible(a), Adjusted(_, b))    => D.order(a, b)
-      case (Feasible(a), Feasible(b))       => D.order(a, b)
-      case (Feasible(_), Infeasible(_))     => GT
-      case (Infeasible(_), Adjusted(_, _))  => LT
-      case (Infeasible(_), Feasible(_))     => LT
-      case (Infeasible(a), Infeasible(b)) =>
-        if (xv == yv) D.order(a, b)
-        else I.order(xv, yv)
-    }
-
-  def objectiveOrder[A] = new Order[Objective[A]] {
-    def order(x: Objective[A], y: Objective[A]) =
-      (x.fitness, y.fitness) match {
-        case (-\/(f1), -\/(f2)) => fitCompare(f1, f2, x.violationCount, y.violationCount)
-        case (\/-(xs), \/-(ys)) =>
-          val z = xs.zip(ys)
-          val x2 = z.forall {
-            case (a, b) =>
-              val r = fitCompare(a, b, x.violationCount, y.violationCount)
-              r == GT || r == EQ
-          }
-          val y2 = z.exists {
-            case (a, b) => fitCompare(a, b, x.violationCount, y.violationCount) == GT
-          }
-
-          if (x2 && y2) GT else if (x2) EQ else LT
-
-        case _ => EQ
-      }
-  }
-
+  val D = Order[Double]
 }
--- tests/src/test/scala/cilib/FitnessTests.scala
+++ tests/src/test/scala/cilib/FitnessTests.scala
@@ -3,6 +3,8 @@ package cilib
 import spire.implicits._
 
 import scalaz._
+import scalaz.Ordering._
+import scalaz.scalacheck.ScalazArbitrary._
 
 import org.scalacheck._
 import org.scalacheck.Prop._
@@ -10,6 +12,9 @@ import org.scalacheck.Gen._
 import org.scalacheck.Arbitrary._
 
 object FitnessTest extends Properties("Fitness") {
+
+  trait Single
+  trait Multi
 
   val genFeasibleFitness: Gen[Fit] =
     Gen.choose(-1000, 1000).map(x => Feasible(x.toDouble))
@@ -34,19 +39,26 @@ object FitnessTest extends Properties("Fitness") {
       )
     } yield constraint
 
-  def objectiveGen: Gen[Objective[Int]] =
+  def singleObjectiveGen: Gen[Objective[Int] @@ Single] =
     for {
-      violationCount <- Gen.choose(1, 3)
+      violationCount <- Gen.choose(1,5)
       violations <- Gen.listOfN(violationCount, simpleViolationGen)
       obj <- Gen.oneOf(
-        genFeasibleFitness.map(f => Objective.single(f, List.empty[Constraint[Int]])),
-        genPenaltyFitness.map(f => Objective.single(f, violations)),
-        genInfeasibleFitenss.map(f => Objective.single(f, violations))
+        genFeasibleFitness.map(f => Tag[Objective[Int], Single](Objective.single(f, List.empty[Constraint[Int]]))),
+        genPenaltyFitness.map(f => Tag[Objective[Int], Single](Objective.single(f, violations))),
+        genInfeasibleFitenss.map(f => Tag[Objective[Int], Single](Objective.single(f, violations)))
       )
     } yield obj
 
+  implicit def arbSingleObjective = Arbitrary { singleObjectiveGen }
 
-  implicit def arbObjective = Arbitrary { objectiveGen }
+
+  def multiObjectiveGen: Gen[Objective[Int] @@ Multi] =
+    for {
+      nel <- arbitrary[NonEmptyList[Objective[Int] @@ Single]]
+    } yield Tag[Objective[Int], Multi](Objective.multi(nel.map(Tag.unwrap)))
+
+  implicit def arbMultiObjective = Arbitrary { multiObjectiveGen }
 
   implicit def idFitness: Fitness[Option, Objective[Int], Int] =
     new Fitness[Option, Objective[Int], Int] {
@@ -60,28 +72,61 @@ object FitnessTest extends Properties("Fitness") {
         Some(x)
     }
 
-  def better(opt: Opt)(x: Option[Objective[Int]], y: Option[Objective[Int]]) =
+  def better(opt: Opt)(x: Option[Objective[Int]], y: Option[Objective[Int]]): Option[Objective[Int]] =
     (x, y) match {
       case (Some(a), Some(b)) =>
-        if (Comparison.fitter(a, b).apply(Comparison.quality(opt))) x else y
-      case (None, Some(_)) => y
-      case (Some(_), None) => x
-      case _ => x
+        (a.fitness, b.fitness) match {
+          case (-\/(f1), -\/(f2)) =>
+            if (Comparison.fitCompare(opt)(f1, f2, a.violationCount, b.violationCount) == GT) x else y
+          case (\/-(f1), \/-(f2)) =>
+            if (Comparison.multiFitCompare(opt)(f1, f2, a.violationCount, b.violationCount) == GT) x else y
+          case _ => x
+        }
+      case (None, None) => x
+      case (None, _) => y
+      case (_, None) => x
     }
 
-  property("min") = {
-    def min(x: Option[Objective[Int]], y: Option[Objective[Int]]) = better(Min)(x, y)
+  property("single objective min") = {
+    forAll { (x: Option[Objective[Int] @@ Single], y: Option[Objective[Int] @@ Single]) =>
+      val a = x.map(Tag.unwrap)
+      val b = y.map(Tag.unwrap)
 
-    forAll { (x: Option[Objective[Int]], y: Option[Objective[Int]]) =>
-      Comparison.quality(Min)(x, y) == min(x, y)
+      Comparison.quality(Min)(a, b) == better(Min)(a, b)
     }
   }
 
-  property("max") = {
-    def max(x: Option[Objective[Int]], y: Option[Objective[Int]]) = better(Max)(x, y)
+  property("single objective max") = {
+    forAll { (x: Option[Objective[Int] @@ Single], y: Option[Objective[Int] @@ Single]) =>
+      val a = x.map(Tag.unwrap)
+      val b = y.map(Tag.unwrap)
 
-    forAll { (x: Option[Objective[Int]], y: Option[Objective[Int]]) =>
-      Comparison.quality(Max)(x, y) == max(x, y)
+      Comparison.quality(Max)(a, b) == better(Max)(a, b)
     }
   }
+
+  property("multi objective dominance min") = {
+    def min(x: Option[Objective[Int]], y: Option[Objective[Int]]) =
+      better(Min)(x, y)
+
+    forAll { (x: Option[Objective[Int] @@ Multi], y: Option[Objective[Int] @@ Multi]) =>
+      val a = x.map(Tag.unwrap)
+      val b = y.map(Tag.unwrap)
+
+      Comparison.quality(Min)(a, b) == min(a, b)
+    }
+  }
+
+  property("multi objective dominance max") = {
+    def max(x: Option[Objective[Int]], y: Option[Objective[Int]]) =
+      better(Max)(x, y)
+
+    forAll { (x: Option[Objective[Int] @@ Multi], y: Option[Objective[Int] @@ Multi]) =>
+      val a = x.map(Tag.unwrap)
+      val b = y.map(Tag.unwrap)
+
+      Comparison.quality(Max)(a, b) == max(a, b)
+    }
+  }
+
 }
