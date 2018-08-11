07fc81274d5ee86e4d7127d3574a4fff5b3399d6
Kyle Erwin <kyle.erwin24@gmail.com>
2018-08-11 16:46:59 +0200

    scalafmt

--- example/src/main/scala/cilib/example/DynamicProblem.scala
+++ example/src/main/scala/cilib/example/DynamicProblem.scala
@@ -19,38 +19,13 @@ object DynamicProblem extends SafeApp {
   // that increases over time. This causes the most optimum area to shift
   // from the upper bound of each dimension to the lower bound.
 
-  override val runc: IO[Unit] = {
-    val t = Runner.foldStep(
-      env,
-      RNG.init(12L),
-      swarm,
-      Runner.staticAlgorithm("gbestPSO", iter),
-      problemStream,
-      (x: NonEmptyList[Particle[Mem[Double], Double]], _: Eval[NonEmptyList, Double]) =>
-        RVar.pure(x)
-    )
-
-    putStrLn(t.take(1000).runLast.unsafePerformSync.toString)
-  }
+  // Our fitness function with a bias.
+  def fitnessFunction(bias: Double)(l: NonEmptyList[Double]): Double =
+    Math.abs(l.map(x => x - bias).suml)
 
   val bounds = Interval(0.0, 10.0) ^ 30
   val env =
-    Environment(cmp = Comparison.dominance(Max), eval = Eval.unconstrained(fitnessFunction(0.0)))// Start with no bias
-    // Define the guides for our PSO algorithms
-  val cognitive = Guide.pbest[Mem[Double], Double]
-  val social = Guide.gbest[Mem[Double]]
-  val gbestPSO = gbest(0.729844, 1.496180, 1.496180, cognitive, social)
-    // Define the initial swarm
-    val swarm =
-    Position.createCollection(PSO.createParticle(x => Entity(Mem(x, x.zeroed), x)))(bounds, 20)
-    // Define iterators for our algorithm
-    val iter = Iteration.sync(gbestPSO)
-  // How frequently we want to update the evaluation method
-  val freq = Env.frequency(10)
-  // problem takes in the name of the problem, the initial state and a method to update the
-  // evaluation process.
-  val problemStream =
-    Runner.problem[Double, Double]("spherical", Dist.stdUniform, dynamicUpdate)(freq, RNG.init(12L))
+    Environment(cmp = Comparison.dominance(Max), eval = Eval.unconstrained(fitnessFunction(0.0))) // Start with no bias
 
   // Our gbest PSO algorithm with bounds a checking mechanism (updatePBestBounds)
   def gbest[S](
@@ -73,6 +48,19 @@ object DynamicProblem extends SafeApp {
           updated <- updatePBestBounds(p3)
         } yield updated
 
+  // Define the guides for our PSO algorithms
+  val cognitive = Guide.pbest[Mem[Double], Double]
+  val social = Guide.gbest[Mem[Double]]
+
+  val gbestPSO = gbest(0.729844, 1.496180, 1.496180, cognitive, social)
+
+  // Define the initial swarm
+  val swarm =
+    Position.createCollection(PSO.createParticle(x => Entity(Mem(x, x.zeroed), x)))(bounds, 20)
+
+  // Define iterators for our algorithm
+  val iter = Iteration.sync(gbestPSO)
+
   // A method to update update the evaluation method that takes in some state,
   // in oue case a double.
   def dynamicUpdate(state: Double): RVar[(Double, Eval[NonEmptyList, Double])] =
@@ -82,7 +70,25 @@ object DynamicProblem extends SafeApp {
       (newState, eval)
     })
 
-  // Our fitness function with a bias.
-  def fitnessFunction(bias: Double)(l: NonEmptyList[Double]): Double =
-    Math.abs(l.map(x => x - bias).suml)
+  // How frequently we want to update the evaluation method
+  val freq = Env.frequency(10)
+
+  // problem takes in the name of the problem, the initial state and a method to update the
+  // evaluation process.
+  val problemStream =
+    Runner.problem[Double, Double]("spherical", Dist.stdUniform, dynamicUpdate)(freq, RNG.init(12L))
+
+  override val runc: IO[Unit] = {
+    val t = Runner.foldStep(
+      env,
+      RNG.init(12L),
+      swarm,
+      Runner.staticAlgorithm("gbestPSO", iter),
+      problemStream,
+      (x: NonEmptyList[Particle[Mem[Double], Double]], _: Eval[NonEmptyList, Double]) =>
+        RVar.pure(x)
+    )
+
+    putStrLn(t.take(1000).runLast.unsafePerformSync.toString)
+  }
 }
