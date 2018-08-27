060c7a5bc8e9f96073753146076aeff5501bd658
Kyle Erwin <kyle.erwin24@gmail.com>
2018-08-27 20:53:09 +0200

    PrettyPrinter object with opinionated implicits

--- io/src/main/scala/PrettyPrinter.scala
+++ io/src/main/scala/PrettyPrinter.scala
@@ -4,29 +4,41 @@ package io
 import org.typelevel.paiges._
 import scalaz._
 import Scalaz._
+import spire.math.Interval
+
+sealed trait PrettyPrinter[A] {
+    def toDoc(x: A): Doc
+}
 
 object PrettyPrinter {
 
-    implicit class positionPrinterOps(p: Position[Double]){
-        def prettyPrint: Doc = {
-            val pos = Doc.text("Position: ") +
-                Doc.intercalate(Doc.text(", "), p.pos.toList.map(x => Doc.text(x.toString))) +
-                Doc.line
+    def apply[A](x: A)(implicit prettyPrinter: PrettyPrinter[A]): Doc = prettyPrinter.toDoc(x)
 
-            val bounds = if(p.boundary.toList.forall(_ == p.boundary.head)) {
-                Doc.text("All points in the position share the same bounds of " + p.boundary.head.toString())
-            } else {
-                Doc.text("Bounds: ") +
-                    Doc.intercalate(Doc.text(", "), p.boundary.toList.map(x => Doc.text(x.toString()))) +
+    implicit val boundsPrettyPrinter: PrettyPrinter[NonEmptyList[Interval[Double]]] =
+        new PrettyPrinter[NonEmptyList[Interval[Double]]] {
+            def toDoc(bounds: NonEmptyList[Interval[Double]]): Doc =
+                Doc.intercalate(Doc.paragraph(", "), bounds.toList.map(x => Doc.paragraph(x.toString()))) +
                     Doc.line
-            }
-
-            val solution = Lenses._singleFitness[Double].composePrism(Lenses._feasible).headOption(p) match {
-                    case None        => Doc.empty
-                    case Some(value) => Doc.text("Solution: " + value.toString)
-                }
-
         }
-    }
+
+    implicit val nelPrettyPrinter: PrettyPrinter[NonEmptyList[Double]] =
+        new PrettyPrinter[NonEmptyList[Double]] {
+            def toDoc(bounds: NonEmptyList[Double]): Doc =
+                Doc.intercalate(Doc.paragraph(", "), bounds.toList.map(x => Doc.paragraph(x.toString()))) +
+                    Doc.line
+        }
+
+    implicit val positionPrettyPrinter: PrettyPrinter[Position[Double]] =
+        new PrettyPrinter[Position[Double]] {
+            def toDoc(x: Position[Double]): Doc =
+                Doc.paragraph("Position: ") + nelPrettyPrinter.toDoc(x.pos) + Doc.paragraph("Bounds: ") +
+                    boundsPrettyPrinter.toDoc(x.boundary)
+        }
 
 }
+
+val rng = RNG.init(12L)
+val bounds = Interval(-10.0, 10.0) ^ 3
+val point = Position.createPosition(bounds).eval(rng)
+
+PrettyPrinter(point).render(80)
