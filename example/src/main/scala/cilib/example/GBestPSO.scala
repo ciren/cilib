97e8e6ed911666d4ce6880c294a113cfe234b0ae
Kyle Erwin <kyle.erwin24@gmail.com>
2018-09-05 08:44:45 +0200

    PrettyPrinter object with common EncodeDoc implicits

--- example/src/main/scala/cilib/example/GBestPSO.scala
+++ example/src/main/scala/cilib/example/GBestPSO.scala
@@ -14,36 +14,36 @@ import spire.implicits._
 import spire.math.Interval
 
 object GBestPSO extends SafeApp {
-  val bounds = Interval(-5.12, 5.12) ^ 30
-  val env =
-    Environment(cmp = Comparison.dominance(Min),
-                eval =
-                  Eval.unconstrained(cilib.benchmarks.Benchmarks.spherical[NonEmptyList, Double]))
+    val bounds = Interval(-5.12, 5.12) ^ 30
+    val env =
+        Environment(cmp = Comparison.dominance(Min),
+            eval =
+                Eval.unconstrained(cilib.benchmarks.Benchmarks.spherical[NonEmptyList, Double]))
 
-  // Define a normal GBest PSO and run it for a single iteration
-  val cognitive = Guide.pbest[Mem[Double], Double]
-  val social = Guide.gbest[Mem[Double]]
-  val gbestPSO = gbest(0.729844, 1.496180, 1.496180, cognitive, social)
+    // Define a normal GBest PSO and run it for a single iteration
+    val cognitive = Guide.pbest[Mem[Double], Double]
+    val social = Guide.gbest[Mem[Double]]
+    val gbestPSO = gbest(0.729844, 1.496180, 1.496180, cognitive, social)
 
-  // RVar
-  val swarm =
-    Position.createCollection(PSO.createParticle(x => Entity(Mem(x, x.zeroed), x)))(bounds, 20)
-  val iter = Iteration.sync(gbestPSO)
+    // RVar
+    val swarm =
+        Position.createCollection(PSO.createParticle(x => Entity(Mem(x, x.zeroed), x)))(bounds, 20)
+    val iter = Iteration.sync(gbestPSO)
 
-  val problemStream = Runner.staticProblem("spherical", env.eval)
+    val problemStream = Runner.staticProblem("spherical", env.eval)
 
-  // Our IO[Unit] that runs the algorithm, at the end of the world
-  override val runc: IO[Unit] = {
-    val t = Runner.foldStep(
-      env,
-      RNG.fromTime,
-      swarm,
-      Runner.staticAlgorithm("gbestPSO", iter),
-      problemStream,
-      (x: NonEmptyList[Particle[Mem[Double], Double]], _: Eval[NonEmptyList, Double]) =>
-        RVar.pure(x)
-    )
+    // Our IO[Unit] that runs the algorithm, at the end of the world
+    override val runc: IO[Unit] = {
+        val t = Runner.foldStep(
+            env,
+            RNG.fromTime,
+            swarm,
+            Runner.staticAlgorithm("gbestPSO", iter),
+            problemStream,
+            (x: NonEmptyList[Particle[Mem[Double], Double]], _: Eval[NonEmptyList, Double]) =>
+                RVar.pure(x)
+        )
 
-    putStrLn(t.take(1000).runLast.unsafePerformSync.toString)
-  }
+        putStrLn(t.take(1000).runLast.unsafePerformSync.toString)
+    }
 }
--- io/src/main/scala/PrettyPrinter.scala
+++ io/src/main/scala/PrettyPrinter.scala
@@ -1,44 +1,86 @@
 package cilib
 package io
 
+import cilib.exec.Progress
 import org.typelevel.paiges._
 import scalaz._
 import Scalaz._
 import spire.math.Interval
 
-sealed trait PrettyPrinter[A] {
+@annotation.implicitNotFound(
+    """
+EncodeDoc derivation error for type: ${A}
+Not all members of the type have instances of EncodeDoc defined, or in
+scope. It is recommended to examine the fields of the type and then to
+define an instance of EncodeDoc for the any custom parameter type.""")
+sealed trait EncodeDoc[A] {
     def toDoc(x: A): Doc
 }
 
 object PrettyPrinter {
 
-    def apply[A](x: A)(implicit prettyPrinter: PrettyPrinter[A]): Doc = prettyPrinter.toDoc(x)
+    def apply[A](a: A)(implicit docEncoder: EncodeDoc[A]) =
+        docEncoder.toDoc(a)
 
-    implicit val boundsPrettyPrinter: PrettyPrinter[NonEmptyList[Interval[Double]]] =
-        new PrettyPrinter[NonEmptyList[Interval[Double]]] {
-            def toDoc(bounds: NonEmptyList[Interval[Double]]): Doc =
-                Doc.intercalate(Doc.paragraph(", "), bounds.toList.map(x => Doc.paragraph(x.toString()))) +
-                    Doc.line
+    def createEncoder[A](f: A => Doc) =
+        new EncodeDoc[A] {
+            override def toDoc(x: A): Doc = f(x)
         }
 
-    implicit val nelPrettyPrinter: PrettyPrinter[NonEmptyList[Double]] =
-        new PrettyPrinter[NonEmptyList[Double]] {
-            def toDoc(bounds: NonEmptyList[Double]): Doc =
-                Doc.intercalate(Doc.paragraph(", "), bounds.toList.map(x => Doc.paragraph(x.toString()))) +
-                    Doc.line
-        }
+    // Method for creating docs based on non empty lists
+    private def nelEncoder[A](x: NonEmptyList[A])(implicit docEncoder: EncodeDoc[A]) =
+        Doc.intercalate(Doc.paragraph(", "), x.toList.map(docEncoder.toDoc))
 
-    implicit val positionPrettyPrinter: PrettyPrinter[Position[Double]] =
-        new PrettyPrinter[Position[Double]] {
-            def toDoc(x: Position[Double]): Doc =
-                Doc.paragraph("Position: ") + nelPrettyPrinter.toDoc(x.pos) + Doc.paragraph("Bounds: ") +
-                    boundsPrettyPrinter.toDoc(x.boundary)
-        }
+    // Encoders for basic types
+    implicit val booleanEncodeDoc = createEncoder[Boolean](x => Doc.paragraph(x.toString))
+    implicit val byteEncodeDoc = createEncoder[Byte](x => Doc.paragraph(x.toString))
+    implicit val shortEncodeDoc = createEncoder[Short](x => Doc.paragraph(x.toString))
+    implicit val intEncodeDoc = createEncoder[Int](x => Doc.paragraph(x.toString))
+    implicit val longEncodeDoc = createEncoder[Long](x => Doc.paragraph(x.toString))
+    implicit val floatEncodeDoc = createEncoder[Float](x => Doc.paragraph(x.toString))
+    implicit val doubleEncodeDoc = createEncoder[Double](x => Doc.paragraph(x.toString))
+    implicit val stringEncodeDoc = createEncoder[String](Doc.paragraph)
+
+    // Opinionated implicicts for common types within CIlib
+    implicit val intervalEncodeDoc = createEncoder[Interval[Double]](x => Doc.paragraph(x.toString))
+
+    implicit val memEncoder = createEncoder[Mem[Double]](x => {
+        val feasibleOptic = Lenses._singleFitness[Double].composePrism(Lenses._feasible)
+        Doc.paragraph("Personal Best Position: ") + nelEncoder(x.b.pos) + Doc.line +
+            Doc.paragraph("Personal Best Bounds: ") + nelEncoder(x.b.boundary) + Doc.line +
+            Doc.paragraph("Personal Best Solution: ") + Doc.paragraph(feasibleOptic.headOption(x.b).getOrElse("Infeasible").toString) +
+            Doc.line + Doc.paragraph("Velocity: ") + nelEncoder(x.v.pos)
+    })
+
+    implicit val positionEncodeDoc = createEncoder[Position[Double]](x => {
+        val feasibleOptic = Lenses._singleFitness[Double].composePrism(Lenses._feasible)
+        Doc.paragraph("Position: ") + nelEncoder(x.pos) + Doc.line +
+            Doc.paragraph("Bounds: ") + nelEncoder(x.boundary) + Doc.line +
+            Doc.paragraph("Solution: ") + Doc.paragraph(feasibleOptic.headOption(x).getOrElse("Infeasible").toString)
+    })
+
+    implicit val stdEntityEcodeDoc = createEncoder[Entity[Mem[Double], Double]](x =>
+        Doc.paragraph("Entity") + Doc.line +
+            positionEncodeDoc.toDoc(x.pos) + Doc.line +
+            memEncoder.toDoc(x.state) + Doc.line
+    )
+
+    implicit val nelStdEntityEcodeDoc = createEncoder[NonEmptyList[Entity[Mem[Double], Double]]](x =>
+        Doc.intercalate(Doc.line, x.zipWithIndex.toList map {
+            case (entity: Entity[Mem[Double], Double], index: Int) =>
+                Doc.paragraph(s"Entity ${index + 1}") + Doc.line +
+                    positionEncodeDoc.toDoc(entity.pos) + Doc.line +
+                    memEncoder.toDoc(entity.state) + Doc.line
+        })
+    )
+
+    implicit val progressEcodeDoc = createEncoder[Progress[NonEmptyList[Entity[Mem[Double], Double]]]](x =>
+        Doc.paragraph(s"Algorithm: ${x.algorithm}") + Doc.line +
+            Doc.paragraph(s"Problem: ${x.problem}") + Doc.line +
+            Doc.paragraph(s"Seed Value: ${x.seed}") + Doc.line +
+            Doc.paragraph(s"Iterations: ${x.iteration}") + Doc.line +
+            Doc.paragraph(s"Environment: ${x.env}") + Doc.line + Doc.line +
+            nelStdEntityEcodeDoc.toDoc(x.value)
+    )
 
 }
-
-val rng = RNG.init(12L)
-val bounds = Interval(-10.0, 10.0) ^ 3
-val point = Position.createPosition(bounds).eval(rng)
-
-PrettyPrinter(point).render(80)
