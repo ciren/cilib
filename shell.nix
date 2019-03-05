89bf56ff6b938a359d2b55393ca4f9ccbda24056
Gary Pamparà <gpampara@gmail.com>
2019-03-05 10:49:06 +0200

    Pin nixpkgs

--- shell.nix
+++ shell.nix
@@ -1,8 +1,18 @@
-with import <nixpkgs> {};
-
-stdenv.mkDerivation {
-  name = "java8-env";
-  buildInputs = [
+let
+  pinnedVersion = "18.09";
+  pkgs =
+   import (builtins.fetchTarball {
+     # Descriptive name to make the store path easier to identify
+     name = "nixos-tag-${pinnedVersion}";
+     # URL for nixpks releases on GitHub
+     url = "https://github.com/NixOS/nixpkgs/archive/${pinnedVersion}.tar.gz";
+     # Hash obtained using `nix-prefetch-url --unpack <url>`
+     sha256 = "1ib96has10v5nr6bzf7v8kw7yzww8zanxgw2qi1ll1sbv6kj6zpd";
+   }) {};
+in
+pkgs.mkShell {
+  src = if pkgs.lib.inNixShell then null else pkgs.nix;
+  buildInputs = with pkgs; [
     jdk8
     sbt
   ];
