594171dd2228d81a64e9474b5509e33f8cf57f6f
Gary Pamparà <gpampara@gmail.com>
2019-12-31 09:39:12 +0200

    Remove the docusaurus default side bar items

--- website/sidebars.js
+++ website/sidebars.js
@@ -6,30 +6,30 @@
  */
 
 module.exports = {
-  docs: {
-    Docusaurus: ['doc1', 'doc2', 'doc3'],
-      Tutorial: [
-          'mdoc/community-tutorial/introduction',
-          'mdoc/community-tutorial/setting-up',
-          'mdoc/community-tutorial/rvar',
-          // 'mdoc/community-tutorial/constraint',
-          // 'mdoc/community-tutorial/objective',
-          // 'mdoc/community-tutorial/eval',
-          // 'mdoc/community-tutorial/fit',
-          // 'mdoc/community-tutorial/position',
-          // 'mdoc/community-tutorial/lenses',
-          // 'mdoc/community-tutorial/entity',
-          // 'mdoc/community-tutorial/step',
-          // 'mdoc/community-tutorial/step-with-state',
-          // 'mdoc/community-tutorial/runner',
-          // 'mdoc/community-tutorial/metric-space',
-          // 'mdoc/community-tutorial/selection',
-          // 'mdoc/community-tutorial/benchmarks',
-          // 'mdoc/community-tutorial/ga',
-          // 'mdoc/community-tutorial/pso',
-          // 'mdoc/community-tutorial/de',
-          // 'mdoc/community-tutorial/solutions',
-          // 'mdoc/community-tutorial/links'
-      ],
-  },
+    docs: {
+        //Docusaurus: ['doc1', 'doc2', 'doc3'],
+        Tutorial: [
+            'mdoc/community-tutorial/introduction',
+            'mdoc/community-tutorial/setting-up',
+            'mdoc/community-tutorial/rvar',
+            // 'mdoc/community-tutorial/constraint',
+            // 'mdoc/community-tutorial/objective',
+            // 'mdoc/community-tutorial/eval',
+            // 'mdoc/community-tutorial/fit',
+            // 'mdoc/community-tutorial/position',
+            // 'mdoc/community-tutorial/lenses',
+            // 'mdoc/community-tutorial/entity',
+            // 'mdoc/community-tutorial/step',
+            // 'mdoc/community-tutorial/step-with-state',
+            // 'mdoc/community-tutorial/runner',
+            // 'mdoc/community-tutorial/metric-space',
+            // 'mdoc/community-tutorial/selection',
+            // 'mdoc/community-tutorial/benchmarks',
+            // 'mdoc/community-tutorial/ga',
+            // 'mdoc/community-tutorial/pso',
+            // 'mdoc/community-tutorial/de',
+            // 'mdoc/community-tutorial/solutions',
+            // 'mdoc/community-tutorial/links'
+        ],
+    },
 };
