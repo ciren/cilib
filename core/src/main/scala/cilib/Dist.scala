446d0f685d458755d6ecd978e66871c3016e237a
Gary Pamparà <gpampara@gmail.com>
2021-05-20 07:37:37 +0200

    More cleanup to EphemeralStream usage

--- core/src/main/scala/cilib/Dist.scala
+++ core/src/main/scala/cilib/Dist.scala
@@ -1,6 +1,5 @@
 package cilib
 
-import scalaz._, Scalaz._
 import spire.implicits._
 import spire.math.Interval
 
@@ -8,7 +7,6 @@ import _root_.eu.timepit.refined.auto._
 
 object Dist {
   import RVar._
-  import scalaz.std.AllInstances._
 
   val stdUniform: RVar[Double]     = next[Double]
   val stdNormal: RVar[Double]      = gaussian(0.0, 1.0)
@@ -40,7 +38,7 @@ object Dist {
 
   def gamma(k: Double, theta: Double): RVar[Double] = {
     val n        = k.toInt
-    val gammaInt = stdUniform.replicateM(n).map(_.toList.foldMap(x => -math.log(x)))
+    val gammaInt = stdUniform.replicateM(n).map(_.map(x => -math.log(x)).sum)
     val gammaFrac = {
       val delta = k - n
 
@@ -130,22 +128,29 @@ object Dist {
       .map(x => if (ineg) x._1 - min else min - x._1)
   }
 
-  //private val ZIGNOR_C = 128                 // Number of blocks
+  //private val ZIGNOR_C = 128               // Number of blocks
   private val ZIGNOR_R = 3.442619855899      // Start of the right tail
   private val ZIGNOR_V = 9.91256303526217e-3 // (R * phi(R) + Pr(X>=3)) * sqrt(2/pi)
   private val (blocks, ratios) = {
+    def unfold[A, B](b: => B)(f: B => Option[(A, B)]): Stream[A] =
+      f(b) match {
+        case None         => Stream.empty[A]
+        case Some((a, r)) => a #:: unfold(r)(f)
+      }
+
     val f = math.exp(-0.5 * ZIGNOR_R * ZIGNOR_R)
-    val blocks = {
-      (ZIGNOR_V / f) ##::
-        ZIGNOR_R ##::
-        (EphemeralStream.unfold((126, ZIGNOR_V / f, ZIGNOR_R)) { (a: (Int, Double, Double)) =>
+    val unfoldStart = (126, ZIGNOR_V / f, ZIGNOR_R)
+    val blocks: Stream[Double] = {
+      Stream(ZIGNOR_V / f, ZIGNOR_R) ++
+        (unfold(unfoldStart) { (a: (Int, Double, Double)) =>
           val f = math.exp(-0.5 * a._3 * a._3)
           val v = math.sqrt(-2.0 * math.log(ZIGNOR_V / a._2 + f))
-          if (a._1 == 0) none[(Double, (Int, Double, Double))] else (v, (a._1 - 1, a._3, v)).some
-        } ++ EphemeralStream(0.0))
-    }.toList
+          if (a._1 == 0) None else Some((v, (a._1 - 1, a._3, v)))
+        }) ++ Stream(0.0)
+    }
 
-    (blocks, blocks.apzip(_.drop(1)).map(a => a._1 / a._2))
+    (blocks, blocks.zip(blocks.drop(1)).map(a => a._1 / a._2))
+    //(blocks, ???)//blocks.zip apzip(_.drop(1)).map(a => a._1 / a._2))
   }
 
   def gaussian(mean: Double, dev: Double): RVar[Double] =
