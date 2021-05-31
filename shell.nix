550d065f32a1958b3ad206dcc35a7b5ea14daed1
Gary Pamparà <gpampara@gmail.com>
2021-05-31 07:55:12 +0200

    Fix shell.nix

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 let
   flake-compat = import (builtins.fetchGit "https://github.com/edolstra/flake-compat");
 in
-(flake-compat { src = ./.; }).shellNix.default
+(flake-compat { src = ./.; }).shellNix
 
 
 # { pkgs ? null }:
