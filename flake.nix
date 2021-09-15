051ee78f516d2c6f250b2d7cab6eccb6dff95628
Gary Pamparà <gpampara@gmail.com>
2021-09-15 06:48:15 +0200

    Add nushell to buildInputs

--- flake.nix
+++ flake.nix
@@ -8,6 +8,28 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+
+        nu_plugin_from_parquet =
+          pkgs.rustPlatform.buildRustPackage rec {
+            pname = "nu_plugin_from_parquet";
+            version = "0.0.0";
+
+            src = pkgs.fetchFromGitHub {
+              owner = "jakeswenson";
+              repo = pname;
+              rev = version;
+              sha256 = "1hqps7l5qrjh9f914r5i6kmcz6f1yb951nv4lby0cjnp5l253kps";
+            };
+
+            cargoSha256 = "x6ncQ+H6xHmSOmM0BiGauEEkau/Y2cvFUG32X3HC7J4=";
+
+            meta = with pkgs.lib; {
+              description = "";
+              homepage = "https://github.com/jakeswenson/nu_plugin_from_parquet";
+              license = licenses.mit;
+              maintainers = [];
+            };
+          };
       in
       {
         devShell = pkgs.mkShell {
@@ -16,6 +38,8 @@
             sbt
             yarn
             nodejs
+            nushell
+            nu_plugin_from_parquet
             #node2nix
             #yarn2nix
           ];
