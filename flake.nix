cbfb46cf4b3576065735fa964e46f749dd3f9717
Gary Pamparà <gpampara@gmail.com>
2021-09-18 08:38:45 +0200

    Add nushell to the development environment
    The nushell (while unique in it's own right) allows for working with
    dataframe data sources (such as .csv and .parquet) files in the shell
    without needing to provide additional tools involving Python/Spark
    software stacks.
    
    There is a caveat here that the dataframe operation is relatively new
    so it might not work in all cases. For example, the compression used
    in our parquet sink is not currently supported but it is hoped to be
    included in subsequent releases. The `pqrs` alternative is also
    available (provided that it compiles).

--- flake.nix
+++ flake.nix
@@ -8,28 +8,6 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-
-        nu_plugin_from_parquet =
-          pkgs.rustPlatform.buildRustPackage rec {
-            pname = "nu_plugin_from_parquet";
-            version = "0.0.0";
-
-            src = pkgs.fetchFromGitHub {
-              owner = "jakeswenson";
-              repo = pname;
-              rev = "004993608f34df7cfbb0f7ef6c877a6d276d1715";
-              sha256 = "F4xOkBd9lphEAIFc5S1ai06rvWC/ceSAP13X8TxcWmg=";
-            };
-
-            cargoSha256 = "cc1KtAUN7EDYEFIGZ1QlJlG6ByHC7s4b+njVMUX0OXE=";
-
-            meta = with pkgs.lib; {
-              description = "";
-              homepage = "https://github.com/jakeswenson/nu_plugin_from_parquet";
-              license = licenses.mit;
-              maintainers = [];
-            };
-          };
       in
       {
         devShell = pkgs.mkShell {
@@ -40,8 +18,7 @@
             nodejs
 
             nushell
-            nu_plugin_from_parquet
-            pqrs
+            #pqrs
 
             #node2nix
             #yarn2nix
